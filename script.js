ed48aeee40a3d1070e710e743ea27bfdd32dd350
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-02-29 11:59:14 -0300

    Colors

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 const pixel_board = document.getElementById('pixel-board');
-const pixels = document.querySelectorAll('.pixel');
-let selected = document.getElementsByClassName('selected');
 let colors_pallet = document.querySelectorAll('.color');
 
-  for (let i = 0; i < colors_pallet.length; i++) {
+for (let i = 0; i < colors_pallet.length; i++) {
 
-    colors_pallet[i].addEventListener('click', function(){
-
-      for (let i = 0; i < colors_pallet.length; i++) {
+  colors_pallet[i].addEventListener('click', function(){
+    
+    for (let i = 0; i < colors_pallet.length; i++) {
         if (colors_pallet[i].classList.contains('selected')) {
           colors_pallet[i].className = 'color';
         }
       }
       colors_pallet[i].className = 'color selected';
-
+      
     });
   }
   
 
-//Criando pixels
+  //Criando pixels
 for (let i = 0; i < 25; i++) {
   let newdiv = document.createElement('div');
   newdiv.className = 'pixel';
   pixel_board.append(newdiv);
 }
+const pixels = document.querySelectorAll('.pixel');
+let selected = document.getElementsByClassName('selected');
 
+
+[].forEach.call(pixels, pixel => {
+  pixel.addEventListener('click', function(){
+     pixel.style.backgroundColor = selected[0].getAttribute("id");
+  });
+});
