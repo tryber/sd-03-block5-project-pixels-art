7e10c8c7f6a8e39ea003a27d18ec8b9f3b67e41b
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-02-29 11:40:18 -0300

    Selected class

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const pixel_board = document.getElementById('pixel-board');
-const columns = document.querySelectorAll('.colum');
+const pixels = document.querySelectorAll('.pixel');
+let selected = document.getElementsByClassName('selected');
+let colors_pallet = document.querySelectorAll('.color');
 
+  for (let i = 0; i < colors_pallet.length; i++) {
 
+    colors_pallet[i].addEventListener('click', function(){
+
+      for (let i = 0; i < colors_pallet.length; i++) {
+        if (colors_pallet[i].classList.contains('selected')) {
+          colors_pallet[i].className = 'color';
+        }
+      }
+      colors_pallet[i].className = 'color selected';
+
+    });
+  }
+  
+
+//Criando pixels
 for (let i = 0; i < 25; i++) {
   let newdiv = document.createElement('div');
   newdiv.className = 'pixel';
   pixel_board.append(newdiv);
 }
 
-const pixels = document.querySelectorAll('.pixel');
-console.log(pixels.length);
