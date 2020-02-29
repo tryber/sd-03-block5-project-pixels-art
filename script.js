fdc32996393e98925257baf4c05509d717a2735f
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-02-29 14:43:43 -0300

    critério 2/2

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-const color1 = document.querySelector(".color1");
-const color2 = document.querySelector(".color2");
-const color3 = document.querySelector(".color3");
-const color4 = document.querySelector(".color4");
+const cor1 = document.querySelector('.cor1');
+const cor2 = document.querySelector('.cor2');
+const cor3 = document.querySelector('.cor3');
+const cor4 = document.querySelector('.cor4');
 
 function selectColor(indicador) {
   if (indicador === 1) {
-    color1.className = "color selected color1";
-    color2.className = "color color2";
-    color3.className = "color color3";
-    color4.className = "color color4";
+    cor1.className = 'color selected cor1';
+    cor2.className = 'color cor2';
+    cor3.className = 'color cor3';
+    cor4.className = 'color cor4';
   } else if (indicador === 2) {
-    color1.className = "color color1";
-    color2.className = "color selected color2";
-    color3.className = "color color3";
-    color4.className = "color color4";
+    cor1.className = 'color cor1';
+    cor2.className = 'color selected cor2';
+    cor3.className = 'color cor3';
+    cor4.className = 'color cor4';
   } else if (indicador === 3) {
-    color1.className = "color color1";
-    color2.className = "color color2";
-    color3.className = "color selected color3";
-    color4.className = "color color4";
+    cor1.className = 'color cor1';
+    cor2.className = 'color cor2';
+    cor3.className = 'color selected cor3';
+    cor4.className = 'color cor4';
   } else if (indicador === 4) {
-    color1.className = "color color1";
-    color2.className = "color color2";
-    color3.className = "color color3";
-    color4.className = "color selected color4";
+    cor1.className = 'color cor1';
+    cor2.className = 'color cor2';
+    cor3.className = 'color cor3';
+    cor4.className = 'color selected cor4';
   }
 }
 
-color1.addEventListener("click", function() {
+cor1.addEventListener('click', function() {
   selectColor(1);
 });
 
-color2.addEventListener("click", function() {
+cor2.addEventListener('click', function() {
   selectColor(2);
 });
 
-color3.addEventListener("click", function() {
+cor3.addEventListener('click', function() {
   selectColor(3);
 });
 
-color4.addEventListener("click", function() {
+cor4.addEventListener('click', function() {
   selectColor(4);
 });
