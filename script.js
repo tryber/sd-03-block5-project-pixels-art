eb9b16bdc2b03c0cf6658b510fc2372fc129a61f
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-29 17:44:09 -0300

    Item 6 e 7

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+let corPintura="";
+
 function colorSelector(){
     let corSelecionada=event.srcElement;
+//Limpa a seleção
+    document.getElementsByClassName("cor1")[0].className="color cor1";
+    document.getElementsByClassName("cor2")[0].className="color cor2";
+    document.getElementsByClassName("cor3")[0].className="color cor3";
+    document.getElementsByClassName("cor4")[0].className="color cor4";
+//Seleciona nova cor
     if(document.getElementsByClassName("cor1")[0]==corSelecionada){
-        console.log("preto");
+        corSelecionada.className=corSelecionada.className +" selected";
     }
     else if(document.getElementsByClassName("cor2")[0]==corSelecionada){
-        console.log("vermelho");
+        corSelecionada.className=corSelecionada.className +" selected";
     }
     else if(document.getElementsByClassName("cor3")[0]==corSelecionada){
-        console.log("verde");
-    }
+        corSelecionada.className=corSelecionada.className +" selected";
+   }
     else if(document.getElementsByClassName("cor4")[0]==corSelecionada){
-        console.log("azul");
+        corSelecionada.className=corSelecionada.className +" selected";
+    }
+    corPintura=corSelecionada.className;
+}
+
+function pintaCorSelecionada(){
+    let pixelSelecionado=event.srcElement;
+    if(corPintura=="color cor4 selected"){
+        pixelSelecionado.style.backgroundColor="blue";
     }
 }
