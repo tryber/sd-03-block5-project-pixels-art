5efb04d3b207db237b95a7f0caa3239bdbb043da
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-02-29 14:33:05 -0300

    função seleciona cor

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let color3 = document.querySelector(".color3");
+let color4 = document.querySelector(".color4");
 
+function selectColor(indicador) {
+    if(indicador == 1) {
+        color1.className = "color selected color1";
+        color2.className = "color color2";
+        color3.className = "color color3";
+        color4.className = "color color4";
+    } else if(indicador == 2) {
+        color1.className = "color color1";
+        color2.className = "color selected color2";
+        color3.className = "color color3";
+        color4.className = "color color4";
+    } else if(indicador == 3) {
+        color1.className = "color color1";
+        color2.className = "color color2";
+        color3.className = "color selected color3";
+        color4.className = "color color4";
+    } else if(indicador == 4) {
+        color1.className = "color color1";
+        color2.className = "color color2";
+        color3.className = "color color3";
+        color4.className = "color selected color4";
+    }
+};
+
+color1.addEventListener("click", function() {
+    selectColor(1);
+});
+
+color2.addEventListener("click", function() {
+    selectColor(2);
+});
+
+color3.addEventListener("click", function() {
+    selectColor(3);
+});
+
+color4.addEventListener("click", function() {
+    selectColor(4);
+});
