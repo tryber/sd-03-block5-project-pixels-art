55bec435b7611a3a31b4dd4d6509a7db53c7e985
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-29 19:14:13 -0300

    Adicionando script que troca classe selected para cor selecionada

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.onload = () => {
     sessionStorage.setItem("SelectedColor", "black")
     document.querySelectorAll('.color').forEach(element => element.addEventListener("click", (event) => {
-        sessionStorage.setItem("SelectedColor", event.toElement.attributes.value.value)
+      document.getElementsByClassName("selected")[0].classList.remove("selected")
+      sessionStorage.setItem("SelectedColor", event.toElement.attributes.value.value)
+      document.getElementsByClassName(sessionStorage.getItem("SelectedColor") + "-bg")[0].classList.add("selected")
     }))
     document.querySelectorAll('.pixel').forEach(element => element.addEventListener("click", (event) => {
         event.srcElement.style.backgroundColor = sessionStorage.getItem("SelectedColor")
