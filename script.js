2b63b83ec79c9603f7338527f00f999c1ab2514b
Marco Barbosa <marco.meireles.b@gmail.com>
2020-02-29 19:25:19 -0300

    codeclimate

--- script.js
+++ script.js
@@ -3,38 +3,38 @@ let corPintura = 'color cor1 selected';
 function colorSelector() {
   const corSelecionada = event.srcElement;
 //  Limpa a seleção
-  document.getElementsByClassName('cor1')[0].className = "color cor1";
-  document.getElementsByClassName('cor2')[0].className = "color cor2";
-  document.getElementsByClassName('cor3')[0].className = "color cor3";
-  document.getElementsByClassName('cor4')[0].className = "color cor4";
+  document.getElementsByClassName('cor1')[0].className = 'color cor1';
+  document.getElementsByClassName('cor2')[0].className = 'color cor2';
+  document.getElementsByClassName('cor3')[0].className = 'color cor3';
+  document.getElementsByClassName('cor4')[0].className = 'color cor4';
 //  Seleciona nova cor
-  if(document.getElementsByClassName('cor1')[0] === corSelecionada) {
-     corSelecionada.className = corSelecionada.className +" selected";
+  if (document.getElementsByClassName('cor1')[0] === corSelecionada) {
+    corSelecionada.className = corSelecionada.className + ' selected';
   }
-  else if(document.getElementsByClassName('cor2')[0] === corSelecionada) {
-     corSelecionada.className = corSelecionada.className +" selected";
+  else if (document.getElementsByClassName('cor2')[0] === corSelecionada) {
+    corSelecionada.className = corSelecionada.className + ' selected';
   }
-  else if(document.getElementsByClassName('cor3')[0] === corSelecionada) {
-     corSelecionada.className = corSelecionada.className +" selected";
+  else if (document.getElementsByClassName('cor3')[0] === corSelecionada) {
+    corSelecionada.className = corSelecionada.className + ' selected';
   }
-  else if(document.getElementsByClassName('cor4')[0] === corSelecionada) {
-     corSelecionada.className = corSelecionada.className +" selected";
+  else if (document.getElementsByClassName('cor4')[0] === corSelecionada) {
+    corSelecionada.className = corSelecionada.className + ' selected';
   }
   corPintura = corSelecionada.className;
 }
 
 function pintaCorSelecionada() {
   let pixelSelecionado = event.srcElement;
-  if(corPintura === "color cor1 selected") {
-     pixelSelecionado.style.backgroundColor = "black";
+  if(corPintura === 'color cor1 selected') {
+    pixelSelecionado.style.backgroundColor = "black";
   }
-  if(corPintura === "color cor2 selected") {
-     pixelSelecionado.style.backgroundColor = "red";
+  if(corPintura === 'color cor2 selected') {
+    pixelSelecionado.style.backgroundColor = "red";
   }
-  if(corPintura === "color cor3 selected") {
-     pixelSelecionado.style.backgroundColor = "green";
+  if(corPintura === 'color cor3 selected') {
+    pixelSelecionado.style.backgroundColor = "green";
   }
-  if(corPintura === "color cor4 selected") {
-     pixelSelecionado.style.backgroundColor = "blue";
+  if(corPintura === 'color cor4 selected') {
+    pixelSelecionado.style.backgroundColor = "blue";
   }
 }
