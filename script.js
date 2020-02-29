cccd207118eeecd8eaf338f6709f4d82fc85edd9
Henrique Rezende <henriquerezendec@gmail.com>
2020-02-29 17:55:33 -0300

    Adicionando script que troca pixels de cor

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(document.getElementById("color-palette"))
+window.onload = () => {
+    sessionStorage.setItem("SelectedColor", "black")
+    document.querySelectorAll('.color').forEach(element => element.addEventListener("click", (event) => {
+        sessionStorage.setItem("SelectedColor", event.toElement.attributes.value.value)
+    }))
+    document.querySelectorAll('.pixel').forEach(element => element.addEventListener("click", (event) => {
+        event.srcElement.style.backgroundColor = sessionStorage.getItem("SelectedColor")
+    }));
+}
