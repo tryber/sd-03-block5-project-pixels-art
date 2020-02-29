0bdf000ddb2478958f304caa24564f6d0b73e15c
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-02-29 10:20:24 -0300

    pixels

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const pixel_board = document.getElementById('pixel-board');
+const columns = document.querySelectorAll('.colum');
 
 
-for (let i = 1; i <= 25; i++) {
-    let newdiv = document.createElement('div');
-    newdiv.className = 'pixel';
-    pixel_board.append(newdiv);
-    
-    if (i%5 == 0) {
-      let br = document.createElement('br');
-      pixel_board.append(br) 
-    }
+for (let i = 0; i < 25; i++) {
+  let newdiv = document.createElement('div');
+  newdiv.className = 'pixel';
+  pixel_board.append(newdiv);
 }
+
+const pixels = document.querySelectorAll('.pixel');
+console.log(pixels.length);
