918a1ea9e9f7e4f9c1dc33971fc4019d6e34d231
DevT <aguiarthiag@gmail.com>
2020-03-01 17:52:49 -0300

    Aplicando requisito 8

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-window.onload = function(){
+window.onload = function () {
     this.sessionStorage.setItem("uColor", "black")
     let colorArray = ["black", "red", "green", "blue"]
     let palColor = document.getElementsByClassName("color")
-    for (let i = 0; i < colorArray.length; i++){
+    for (let i = 0; i < colorArray.length; i++) {
         palColor[i].style.backgroundColor = colorArray[i];
-        palColor[i].addEventListener('click', function(){
+        palColor[i].addEventListener('click', function () {
+            let selected = document.getElementsByClassName("selected")
+            selected[0].classList.remove("selected")
             let bgcolor = palColor[i].style.backgroundColor
             sessionStorage.setItem("uColor", bgcolor)
+            palColor[i].classList.add("selected")
+            console.log(palColor[i].classList)
         })
     }
 
     let pixels = document.getElementsByClassName("pixel")
 
-    for (let x = 0; x < pixels.length; x++){
+    for (let x = 0; x < pixels.length; x++) {
 
 
-        pixels[x].addEventListener('click', function(){
+        pixels[x].addEventListener('click', function () {
             let coloring = sessionStorage.getItem("uColor")
             pixels[x].style.backgroundColor = coloring;
         })
@@ -24,6 +28,6 @@ window.onload = function(){
 
 
 
- 
+
 
 }
