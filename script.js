fbdf3d91d1d20e91959d765b0e01b2ac73ea61eb
wscatao <wscatao@gmail.com>
2020-03-01 19:16:56 -0300

    Otimizado em funcoes a configuracao de storage e de classes css

--- script.js
+++ script.js
@@ -13,35 +13,23 @@ let pixel = document.querySelectorAll(".pixel")
 
 divCores[0].addEventListener("click", function(event){
     corEscolhida = divCores[0].style.backgroundColor
-    divCores[ultimaCorEscolhida].classList.remove("selected")
-    divCores[0].classList.add("selected")
-    localStorage.clear()
-    localStorage.setItem("color","0")
-    ultimaCorEscolhida = parseInt(localStorage.getItem("color"))
+    arrumarClasseCss(ultimaCorEscolhida,0)
+    setLocalStorage("0")
 })
 divCores[1].addEventListener("click", function(event){
     corEscolhida = divCores[1].style.backgroundColor
-    divCores[ultimaCorEscolhida].classList.remove("selected")
-    divCores[1].classList.add("selected")
-    localStorage.clear()
-    localStorage.setItem("color","1")
-    ultimaCorEscolhida = parseInt(localStorage.getItem("color"))    
+    arrumarClasseCss(ultimaCorEscolhida,1)
+    setLocalStorage("1")    
 })
 divCores[2].addEventListener("click", function(event){
     corEscolhida = divCores[2].style.backgroundColor
-    divCores[ultimaCorEscolhida].classList.remove("selected")
-    divCores[2].classList.add("selected")
-    localStorage.clear()
-    localStorage.setItem("color","2")
-    ultimaCorEscolhida = parseInt(localStorage.getItem("color")) 
+    arrumarClasseCss(ultimaCorEscolhida,2)
+    setLocalStorage("2")
 })
 divCores[3].addEventListener("click", function(event){
     corEscolhida = divCores[3].style.backgroundColor
-    divCores[ultimaCorEscolhida].classList.remove("selected")
-    divCores[3].classList.add("selected")
-    localStorage.clear()
-    localStorage.setItem("color","3")
-    ultimaCorEscolhida = parseInt(localStorage.getItem("color"))    
+    arrumarClasseCss(ultimaCorEscolhida,3)
+    setLocalStorage("3")
 })
 
 // funções
@@ -60,15 +48,23 @@ function setCorPadrao(){
     ultimaCorEscolhida = parseInt(localStorage.getItem("color"))
 }
 
-function pickPaletteColor() {
-    console.log(corEscolhida)
+function arrumarClasseCss (ultimaCor,refCorAtual) {
+    divCores[ultimaCor].classList.remove("selected")
+    divCores[refCorAtual].classList.add("selected")
+    // return alert("Removido classe selected do ultimo elemento e adicionado ao novo escolhido")
+}
+
+function setLocalStorage (refCorAtual) {
+    localStorage.clear()
+    localStorage.setItem("color",refCorAtual)
+    ultimaCorEscolhida = parseInt(localStorage.getItem("color"))
+    // return alert("Efetuado limpeza no storage e setado a cor escolhida")
 }
 
 // execução de Funções ao carregar a página
 
 setDeCores()
 setCorPadrao()
-// setEventListener()
 
 
 
