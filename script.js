22ce35183598ead9d6f13cd3bfde31088536089d
wscatao <wscatao@gmail.com>
2020-03-01 15:46:26 -0300

    Criando a tabela de pixels

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+// variaveis Constantes
+
+const preto = "rgb(0, 0, 0)"
+const vermelho = "rgb(255, 0, 0)"
+const azul = "rgb(0, 0, 255)"
+const verde = "rgb(0, 128, 0)"
+
+
 // elementos
 
 let divCores = document.querySelectorAll(".color")
 console.log(divCores)
 
+
 // funções
 
 function setDeCores(){
-    let preto = "rgb(0, 0, 0)"
-    let vermelho = "rgb(255, 0, 0)"
-    let azul = "rgb(0, 0, 255)"
-    let verde = "rgb(0, 128, 0)"
     let paletaCores = [preto,vermelho,azul,verde]
-
     for (let i = 0; i<divCores.length; i+=1) {
         divCores[i].style.backgroundColor = paletaCores[i]
     }
 }
 
-// setCores na Paleta
+// execução de Funções
 
 setDeCores()
 
