6236a6add16fefa02a163cd098305ed80b1d0525
wscatao <wscatao@gmail.com>
2020-03-01 17:33:55 -0300

    Adicionado classe selected ao carregar pagina padrao na cor preta

--- script.js
+++ script.js
@@ -4,24 +4,40 @@ const preto = "rgb(0, 0, 0)"
 const vermelho = "rgb(255, 0, 0)"
 const azul = "rgb(0, 0, 255)"
 const verde = "rgb(0, 128, 0)"
+const paletaCores = [preto,vermelho,azul,verde]
 
 
 // elementos
 
 let divCores = document.querySelectorAll(".color")
 console.log(divCores)
+console.log(divCores[0])
 
+let body = document.querySelector("body")
+console.log(body)
+
+let pixel = document.querySelectorAll(".pixel")
+console.log(pixel)
 
 // funções
 
 function setDeCores(){
-    let paletaCores = [preto,vermelho,azul,verde]
     for (let i = 0; i<divCores.length; i+=1) {
         divCores[i].style.backgroundColor = paletaCores[i]
     }
 }
 
-// execução de Funções
+function setCorPadrao(){
+    divCores[0].classList.remove("color")
+    divCores[0].classList.add("selected")
+    divCores[0].style.backgroundColor = preto
+}
+
+
+
+// execução de Funções ao carregar a página
 
 setDeCores()
+setCorPadrao()
+
 
