43d25b91aaab333901c5ebc2970884d1683c4e09
lcdlima <leticia.duarte.lima@gmail.com>
2020-03-01 19:45:17 -0300

    First trial of main requirements

---
+++ script.js
@@ -0,0 +1,15 @@
+let colors = document.querySelectorAll(".color");
+for (let i = 0; i < colors.length; i +=1 ) {
+    colors[i].addEventListener("click", function() {
+        document.querySelector(".selected").classList.remove("selected");
+        colors[i].classList.add("selected");
+    });
+}
+let pixels = document.querySelectorAll(".pixel");
+for (let i = 0; i < pixels.length; i +=1 ) {
+    pixels[i].addEventListener("click", function(){
+        let colorSquare = document.querySelector(".selected");
+        let color = colorSquare.id;
+        pixels[i].style.backgroundColor = color;   
+     })
+}
