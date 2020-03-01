33e3b3aa7f4783a40cd4bb76ab929938b1eb0c43
DevT <aguiarthiag@gmail.com>
2020-03-01 17:31:02 -0300

    atualização visando requisitos obrigatórios

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 window.onload = function(){
+    this.sessionStorage.setItem("uColor", "black")
     let colorArray = ["black", "red", "green", "blue"]
     let palColor = document.getElementsByClassName("color")
-    for (let i = 0; i<colorArray.length; i++){
+    for (let i = 0; i < colorArray.length; i++){
         palColor[i].style.backgroundColor = colorArray[i];
+        palColor[i].addEventListener('click', function(){
+            let bgcolor = palColor[i].style.backgroundColor
+            sessionStorage.setItem("uColor", bgcolor)
+        })
     }
+
+    let pixels = document.getElementsByClassName("pixel")
+
+    for (let x = 0; x < pixels.length; x++){
+
+
+        pixels[x].addEventListener('click', function(){
+            let coloring = sessionStorage.getItem("uColor")
+            pixels[x].style.backgroundColor = coloring;
+        })
+
+    }
+
+
 
  
 
