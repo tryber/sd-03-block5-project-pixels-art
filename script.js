45101dc5a6fa77e510925f5da45b99f56b0b02be
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-02 15:32:58 -0300

    Tirando if para teste

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 window.onload = () => {
+  // Faz com que cor Preta seja a escolhida ao carregar a página
   sessionStorage.setItem("SelectedColor", "black");
+
+  // Faz com que a cor selecionada pelo usuário seja definida para colorir
   document.querySelectorAll('.color').forEach(element => element.addEventListener("click", (event) => {
     document.getElementsByClassName("selected")[0].classList.remove("selected");
     sessionStorage.setItem("SelectedColor", event.toElement.attributes.value.value);
     document.getElementsByClassName(sessionStorage.getItem("SelectedColor") + "-bg")[0].classList.add("selected");
   }));
+
+  // Função que adiciona um listener para cada elemento com classe pixel que colore o pixel clicado
   const addListenerColor = () => document.querySelectorAll('.pixel').forEach(element => element.addEventListener("click", (event) => event.srcElement.style.backgroundColor = sessionStorage.getItem("SelectedColor")));
+
+  // Adiciona o listener ao inicializar a página
   addListenerColor()
+
+  // Adiciona listener que colore de branco todos os pixels da página
   document.getElementById("clear-board").addEventListener("click", () => document.querySelectorAll('.pixel').forEach(element => element.style.backgroundColor = "White"))
+
 
   document.getElementById('generate-board').addEventListener("click", () => {
 
 
     let size = document.getElementById('board-size').value;
-    if(size>4 && size<51){
+    // if(size>4 && size<51){
       document.querySelector('#pixel-board').remove()
 
       let board = document.createElement("div");
@@ -31,6 +41,6 @@ window.onload = () => {
 
         addListenerColor()
       };
-    }
+    // }
   });
 }
