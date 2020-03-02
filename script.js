8759f0114d475a017d0826781bb6813d22244a4c
Gabriel Lucas <dreadpool38@gmail.com>
2020-03-02 12:21:18 -0300

    adicionado componentes bonus

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+//Inicia Variáveis
+let selectElement =document.querySelector('.selected');
+let selectedColor = window.getComputedStyle(selectElement,null).getPropertyValue("background-color");
+let colorNodes = document.getElementsByClassName("color");
+let boardNodes = document.getElementsByClassName("pixel");
 
-let selectedColor = window.getComputedStyle(document.querySelector('.selected'),null).getPropertyValue("background-color");
-
+//inicia funções
+function clearBoard() {
+  for (pixel of boardNodes){
+    pixel.style.backgroundColor = 'white';
+  }
+}
 
 function getColor(e){
 
-
+  selectElement.className = "color";
+  selectElement = e.target;
   selectedColor = window.getComputedStyle(e.target,null).getPropertyValue("background-color");
   e.target.className = "color selected"
 }
@@ -14,12 +24,26 @@ function setColor(e){
 
 }
 
-let colorNodes = document.getElementsByClassName("color");
-let boardNodes = document.getElementsByClassName("pixel");
+function randomColor() {
+  let color = Math.floor(Math.random()*16777215).toString(16);
 
+  return "#" + color;
+
+}
+
+
+//adiciona event listeners nos botões
 for (node of colorNodes){
   node.addEventListener('click', getColor)
 }
 for (pixel of boardNodes){
     pixel.addEventListener('click', setColor)
   }
+
+//escolhe cores aleatórias para as outras paletas
+document.getElementById("blue").style.backgroundColor = randomColor();
+document.getElementById("red").style.backgroundColor = randomColor();
+document.getElementById("green").style.backgroundColor = randomColor();
+
+
+
