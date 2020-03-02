8632781b081175ea787654d56ad945827fbf7acf
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-02 16:38:42 -0300

    cores

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload= function(){
+  
+}
+
+/*let botao= document.getElementsByClassName("generate-board")[0];
+function verBotao(){
+   let classe=document.getElementsByClassName("board-size")[0];
+   let val=classe.value;
+   let criar= document.createElement("section")
+   criar.className("classe")
+   for (let i in val){
+      document.getElementsByClassName("classe")[i].appendChild("div")
+   }
+}
+botao.addEventListener("click", verBotao)
+ let select= document.getElementsByClassName("selected")[0];
+   let cor2= document.getElementsByClassName("azul")[0];
+   let cor3= document.getElementsByClassName("branco")[0];
+   let cor4= document.getElementsByClassName("verde")[0];
+   function trocaCor(){
+      
+   }
+   select.addEventListener("click", trocaCor)
+   cor2.addEventListener("click", trocaCor)
+   cor3.addEventListener("click", trocaCor)
+   cor4.addEventListener("click", trocaCor)
+*/
