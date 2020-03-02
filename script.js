e45235f5b8290474f2b3016532e3b7936b338a4e
natox <alvares.renato@hotmail.com>
2020-03-02 18:26:38 -0300

    Grid with NXN, color paint and selector working

---
+++ script.js
@@ -0,0 +1,37 @@
+const n = 7;
+function makeGrid(n) {
+  const where = document.getElementById('pixel-board');
+  where.appendChild(document.createElement('table'))
+  for (let i = 0; i < n; i++) {
+    let col= document.createElement('td');
+    const table = document.getElementsByTagName('table')[0]
+    col.id = 'col_' + JSON.stringify(i);
+
+    table.appendChild(col)
+    for (let j = 0; j < n; j++) {
+      let row = document.createElement('div')
+      row.className = 'pixel';
+      row.id = 'pixel_' + i + '_' + j
+      let td = document.getElementsByTagName('td')[i]
+      td.appendChild(row)
+    }
+  }
+}
+makeGrid(n)
+const buttons = document.getElementsByTagName('button')
+
+for(let i =0; i < buttons.length; i++){
+  buttons[i].addEventListener('click', function(){
+  const selectedColor = buttons[i].style.backgroundColor;
+  console.log(selectedColor);
+  sessionStorage.color = selectedColor;
+  })
+}
+const pix = document.getElementsByClassName('pixel');
+
+for(let i =0; i < pix.length; i++){
+  pix[i].addEventListener('click', function(){
+  pix[i].style.backgroundColor = sessionStorage.color;
+  console.log(selectedColor)
+  })
+}
