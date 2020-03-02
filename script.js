8732ab42a06287e1ce8a079674d8d96b198d46de
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-02 17:59:53 -0300

    penúltimo requisito tentativa

--- script.js
+++ script.js
@@ -14,6 +14,51 @@ function insertColor(event) {
   event.target.style.backgroundColor = classColor;
 }
 
+function clearBoard() {
+  const board = document.querySelectorAll('td');
+
+  for (const i of board) {
+    i.style.backgroundColor = 'white';
+  }
+}
+
+
+function removeSquare() {
+  const rows = document.getElementsByTagName('tr');
+
+  for (let index = (rows.length - 1); index >= 0; index -= 1) {
+    rows[index].remove();
+  }
+}
+
+function makeSquare(N) {
+  const tbody = document.getElementById('pixel-board').firstElementChild;
+
+  for (let indexRow = 0; indexRow < N; indexRow += 1) {
+    const row = document.createElement('tr');
+    row.classList.add('column');
+
+    for (let indexColumnUnit = 0; indexColumnUnit < N; indexColumnUnit += 1) {
+      const unit = document.createElement('td');
+      unit.classList.add('pixel');
+
+      row.appendChild(unit);
+    }
+    tbody.appendChild(row);
+  }
+}
+
+function squareSize() {
+  let size = document.getElementById('square-size').value;
+  if (size < 5) {
+    size = 5;
+  } else if (size > 50) {
+    size = 50;
+  }
+  removeSquare();
+  makeSquare(size);
+}
+
 function LOADED() {
   const colorsParent = document.getElementById('color-palette');
   colorsParent.addEventListener('click', choseColor);
@@ -22,14 +67,11 @@ function LOADED() {
 
   const square = document.getElementById('pixel-board');
   square.addEventListener('click', insertColor);
-}
 
-function clearBoard() {
-  const board = document.querySelectorAll('td');
-  
-  for (let i of board) {
-    i.style.backgroundColor = 'white';    
-  }
-}
+  const clear = document.getElementById('clear-board');
+  clear.addEventListener('click', clearBoard);
 
+  const scale = document.getElementById('square-size-button');
+  scale.addEventListener('click', squareSize);
+}
 window.onload = LOADED();
