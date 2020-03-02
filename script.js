a318f5cc794ae2e3041bf8b95673fc6b484ea57a
henrique <hbaetaleite@gmail.com>
2020-03-02 18:57:36 -0300

    Atualizando script

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+
+  sessionStorage.setItem("backgroundColor", "black");
+
+  let cores = document.getElementsByClassName('color');
+  let cor = ["black","orange", "blue", "green"];
+
+  for (let i = 0; i < cores.length; i += 1){
+    cores[i].style.backgroundColor = cor[i];
+    cores[i].addEventListener('click', function(){
+    let corAtual = cor[i];
+    sessionStorage.setItem("backgroundColor", corAtual);
+    })
+  };
+
+  let pixel = document.getElementsByClassName('pixel');
+
+  for (let i = 0; i < pixel.length; i += 1){
+    pixel[i].addEventListener('click', function(){
+      pixel[i].style.backgroundColor = sessionStorage.getItem("backgroundColor");
+    })
+  }
+
+
+
+}
