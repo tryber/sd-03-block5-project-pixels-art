cdbc8ddcca1f0dbb1c4e0338f80eb49e8c3e0e22
Gabriel Luiz <gabrielsilva.adm@gmail.com>
2020-03-02 15:58:06 -0300

    atualizando black selected class

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var pixelBoard = document.querySelector("#pixel-board");
 var createElemment = document.createElement("div");
 var corOn = black;
 var divColors = document.querySelector('#color-palette');
-var buceta = document.querySelectorAll('.color');
+var qtdClass = document.querySelectorAll('.color');
 var children = divColors.childNodes; //aqui pega as divs filhas que tem as cores
 
 for(i = 0; i < pixels; i++ ){
@@ -16,7 +16,7 @@ for(i = 0; i < pixels; i++ ){
 for(i=0; i<4; i++){
     var getDiv = divColors.children[i];
     var getColor = divColors.children[i].id; //pega as cores
-    console.log(buceta.length);
+    console.log(qtdClass.length);
     
     getDiv.addEventListener('click', function clicouCor(){
             getColor
