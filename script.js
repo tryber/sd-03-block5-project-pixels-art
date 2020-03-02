d72c0edcf15d779bab3d981a29e53402b970a082
Mateus Moreira <ashuraw@hotmail.com>
2020-03-02 19:55:07 -0300

    v8

--- script.js
+++ script.js
@@ -6,3 +6,22 @@
 //     document.getElementById("pixelBody").appendChild(Cpixel)
 //     }
 // }
+
+let paleta = document.getElementById("color-palette")
+
+let paletaCor = document.getElementsByClassName("color")
+
+let black = document.getElementsByClassName("color")[0]
+let red = document.getElementsByClassName("color")[1]
+let green = document.getElementsByClassName("color")[2]
+let blue = document.getElementsByClassName("color")[3]
+
+function selected(x){
+    for (let i = 0; i < paletaCor.length; i++){
+    paletaCor[i].classList.remove("selected")
+    }
+    x.classList.add("selected")
+}
+
+black.addEventListener("click", selected(black))
+red.addEventListener("click", selected(red))
