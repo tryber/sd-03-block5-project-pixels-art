920e0594a13c1ef13d99846cecedeb61c14e1582
luizfernando <luizfernandotic@gmail.com>
2020-03-02 18:17:27 -0300

    novo requisito resolvido 6

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let primeiracaixa = document.getElementsByClassName("primeiracaixa")[0];
+const primeiracaixa = document.getElementsByClassName('primeiracaixa')[0];
 console.log(primeiracaixa);
 
-console.log("Testando uma string");
+console.log('Testando uma string');
