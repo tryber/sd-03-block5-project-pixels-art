3506deab1a62caf21abfa7adee0dd44f0049a5c7
ilan274 <ilan.herbach@gmail.com>
2020-03-02 17:17:40 -0300

    Bonus requirement

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const pallete1 = document.querySelectorAll('.color')[0];
 const pallete2 = document.querySelectorAll('.color')[1];
 const pallete3 = document.querySelectorAll('.color')[2];
 const pallete4 = document.querySelectorAll('.color')[3];
+const clearAll = document.querySelectorAll('#clear-board')[0];
 
 // Set default selected color as black
 sessionStorage.setItem('color', 'black');
@@ -16,7 +17,7 @@ pallete3.style.backgroundColor = 'blue';
 pallete4.style.backgroundColor = 'grey';
 
 // Color selector
-pallete1.addEventListener ('click', function() {
+pallete1.addEventListener('click', function() {
   sessionStorage.setItem('color', 'black');
   for (let i = 0; i < allColors.length; i += 1) {
     if (i === 0) {
@@ -26,9 +27,9 @@ pallete1.addEventListener ('click', function() {
     }
   }
 });
-pallete2.addEventListener ('click', function() {
+pallete2.addEventListener('click', function() {
   sessionStorage.setItem('color', 'red');
-  for (let i = 0; i < allColors.length; i+=1 ) {
+  for (let i = 0; i < allColors.length; i += 1) {
     if (i === 1) {
       allColors[i].className += ' selected';
     } else {
@@ -36,9 +37,9 @@ pallete2.addEventListener ('click', function() {
     }
   }
 });
-pallete3.addEventListener ('click', function() {
+pallete3.addEventListener('click', function() {
   sessionStorage.setItem('color', 'blue');
-  for (let i = 0; i < allColors.length; i+=1) {
+  for (let i = 0; i < allColors.length; i += 1) {
     if (i === 2) {
       allColors[i].className += ' selected';
     } else {
@@ -46,9 +47,9 @@ pallete3.addEventListener ('click', function() {
     }
   }
 });
-pallete4.addEventListener ('click', function() {
+pallete4.addEventListener('click', function() {
   sessionStorage.setItem('color', 'grey');
-  for (let i = 0; i < allColors.length; i+=1) {
+  for (let i = 0; i < allColors.length; i += 1) {
     if (i === 3) {
       allColors[i].className += ' selected';
     } else {
@@ -62,12 +63,19 @@ const getPixel = document.querySelectorAll('.pixel');
 const pixelSize = document.querySelectorAll('.pixel').length;
 
 // Set initial pixel colors to white
-for (let i = 0; i < pixelSize; i+=1) {
+for (let i = 0; i < pixelSize; i += 1) {
   getPixel[i].style.backgroundColor = 'white';
 }
 
-for (let i = 0; i < pixelSize; i+=1) {
-  getPixel[i].addEventListener ('click', function() {
+for (let i = 0; i < pixelSize; i += 1) {
+  getPixel[i].addEventListener('click', function() {
     getPixel[i].style.backgroundColor = sessionStorage.color;
   });
 }
+
+// Clear all pixels
+clearAll.addEventListener('click', function() {
+  for (let i = 0; i < document.querySelectorAll('.pixel').length; i += 1) {
+    document.querySelectorAll('.pixel')[i].style.backgroundColor = 'white';
+  }
+})
