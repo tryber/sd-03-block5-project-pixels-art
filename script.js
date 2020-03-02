3c5984558d97618c038a53748719c8e4f584eab3
paulo-lins <paulolins100@gmail.com>
2020-03-02 18:50:27 -0300

    Modificaçoes html, css e js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.body.onload = adcElementos;
+var colorPallet = document.getElementById("color-palette") 
+var pixelBoard = document.getElementById("pixel-board") 
 
 function adcElementos () {
     for(var i = 1; i <= 25; i++){
-        var pixelBoard = document.getElementById("pixel-board") 
+        // var pixelBoard = document.getElementById("pixel-board") 
         var pixelDiv = document.createElement("div")
         pixelDiv.className = "pixel"
         pixelDiv.id = `pixel` + `${i}`
@@ -12,20 +14,28 @@ function adcElementos () {
     }
 }
 
+
 var selectedColor = 'black';
 
-function pickId (e) {
+function pickColor (e) {
     var idSelect = (e.target.id);
-    alert(idSelect);
+    var colorPicked = idSelect
+    
+    // idSelect.document.getElementById($"{`idSelect`}")
+    alert(colorPicked);
 }
 
-function changeColor(){
-    selectedColor = idSelect.getElementById.style.backgroundColor;
+function changeColor(e){
+    var idSelect = (e.target.id);
+
+    alert(idSelect)        
 }
 
 
-document.addEventListener ('click', pickId)
-document.addEventListener ('click', changeColor)
+// document.addEventListener ('click', pickId)
+// document.addEventListener ('click', changeColor)
+colorPallet.addEventListener('click',pickColor)
+pixelBoard.addEventListener('click',  changeColor)
 
 
 
