035677fa73106b7e17f44d41f5abc8a61b926b6d
potew <potew0@gmail.com>
2020-03-02 13:52:24 -0300

    Função para pintar os pixels e botão para limpar a grade

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
-var i, 
-paletaCor01 = document.getElementById('cor01'),
-paletaCor02 = document.getElementById('cor02'),
-paletaCor03 = document.getElementById('cor03'),
-paletaCor04 = document.getElementById('cor04');
+document.getElementById('cor01').addEventListener('click', selectCor);
+document.getElementById('cor02').addEventListener('click', selectCor);
+document.getElementById('cor03').addEventListener('click', selectCor);
+document.getElementById('cor04').addEventListener('click', selectCor);
+//Colocando no HTML não funcionou usando evento onclick=(selectCor()).
 
-paletaCor01.addEventListener('click', selectCor1);
-paletaCor02.addEventListener('click', selectCor2);
-paletaCor03.addEventListener('click', selectCor3);
-paletaCor04.addEventListener('click', selectCor4);
-
-let matrizPixels = document.getElementsByClassName('pixel');
+let i, matrizPixels = document.getElementsByClassName('pixel');
 
 for (i = 0 ; i < 25 ; i+=1)
 {
     matrizPixels[i].addEventListener('click', pixelPaint);
 }
 
-
 function pixelPaint() {
-    let corSelecionada = document.getElementsByClassName('selected')[0].style.backgroundColor;
-//    quadrado.style.backgroundColor = corSelecionada;
-    console.log('Quadrado pintado com a cor ' + corSelecionada);
+    //O elemento this neste caso se refere ao elemento (HTML) que chamou a função
+    this.style.backgroundColor = document.getElementsByClassName('selected')[0].style.backgroundColor;
 }
 
-//Para que tenha apenas uma cor com a classe 'selecionada', é necessário procurar a que tem essa classe e remover antes para depois atribuir a classe à nova cor clicada.
-function selectCor1() {
+function selectCor() {
+    //Para que tenha apenas uma cor com a classe 'selecionada', é necessário procurar a que tem essa classe e remover antes para depois atribuir a classe à nova cor clicada.
     document.getElementsByClassName('selected')[0].classList.remove('selected');
-    console.log("A cor selecionada é a preta");
-    paletaCor01.classList.add('selected');
+    console.log("A cor selecionada é " + this.style.backgroundColor);
+    this.classList.add('selected');
 }
 
-function selectCor2() {
-    document.getElementsByClassName('selected')[0].classList.remove('selected');
-    console.log("A cor selecionada é a vermelha");
-    paletaCor02.classList.add('selected');
-}
-
-function selectCor3() {
-    document.getElementsByClassName('selected')[0].classList.remove('selected');
-    console.log("A cor selecionada é a azul");
-    paletaCor03.classList.add('selected');
-}
-
-function selectCor4() {
-    document.getElementsByClassName('selected')[0].classList.remove('selected');
-    console.log("A cor selecionada é a verde");
-    paletaCor04.classList.add('selected');
+function limpaGrade() {
+    for (i = 0 ; i < 25 ; i+=1)
+        matrizPixels[i].style.backgroundColor = "white";
 }
