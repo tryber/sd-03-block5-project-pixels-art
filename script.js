28c159fe5ac05b8cca72ac611188ac6b17c84d4f
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-01 23:26:41 -0300

    teste

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const cor1 = document.querySelector('.cor1');
 const cor2 = document.querySelector('.cor2');
 const cor3 = document.querySelector('.cor3');
 const cor4 = document.querySelector('.cor4');
-const cores = document.querySelectorAll('.color')
-const pixel = document.querySelectorAll('.pixel');
-const limpar = document.querySelector('#clear-board');
+
+let corSelecionada = document.querySelector('.selected')
 
 function selectColor(indicador) {
   if (indicador === 1) {
@@ -12,32 +11,26 @@ function selectColor(indicador) {
     cor2.className = 'color cor2';
     cor3.className = 'color cor3';
     cor4.className = 'color cor4';
+    corSelecionada = document.querySelector('.selected').style.backgroundColor;
   } else if (indicador === 2) {
     cor1.className = 'color cor1';
     cor2.className = 'color selected cor2';
     cor3.className = 'color cor3';
     cor4.className = 'color cor4';
+    corSelecionada = document.querySelector('.selected').style.backgroundColor;
   } else if (indicador === 3) {
     cor1.className = 'color cor1';
     cor2.className = 'color cor2';
     cor3.className = 'color selected cor3';
     cor4.className = 'color cor4';
+    corSelecionada = document.querySelector('.selected').style.backgroundColor;
   } else if (indicador === 4) {
     cor1.className = 'color cor1';
     cor2.className = 'color cor2';
     cor3.className = 'color cor3';
     cor4.className = 'color selected cor4';
-  }
-}
-
-function paintPixel() {
-
-}
-
-function cleanBoard() {
-  for(let i = 0; i < pixel.length; i+=1) {
-    pixel[i].style.backgroundColor = 'white';
-  }
+    corSelecionada = document.querySelector('.selected').style.backgroundColor;
+  } 
 }
 
 cor1.addEventListener('click', function () {
@@ -55,5 +48,3 @@ cor3.addEventListener('click', function () {
 cor4.addEventListener('click', function () {
   selectColor(4);
 });
-
-limpar.addEventListener('click', cleanBoard);
