243c93392b79d9bee171337399861b249607283f
fabemiliano <fab.emiliano@gmail.com>
2020-03-02 15:37:11 -0300

    tried first requistes

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("black").style.backgroundColor = document.getElementById("black").innerHTML
+document.getElementById("green").style.backgroundColor = document.getElementById("green").innerHTML
+document.getElementById("yellow").style.backgroundColor = document.getElementById("yellow").innerHTML
+document.getElementById("red").style.backgroundColor = document.getElementById("red").innerHTML
