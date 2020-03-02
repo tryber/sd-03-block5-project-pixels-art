385b732da3b5621b3ecb8b0ba1d447638a57f5ca
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-02 18:17:24 -0300

    code climate_18.17

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-
-c1 = document.getElementById("c1");
+let c1 = document.getElementById('c1');
 c1.addEventListener('click', cor1);
 
-c2 = document.getElementById("c2");
+let c2 = document.getElementById('c2');
 c2.addEventListener('click', cor2);
 
-c3 = document.getElementById("c3");
+let c3 = document.getElementById('c3');
 c3.addEventListener('click', cor3);
 
-c4 = document.getElementById("c4");
+let c4 = document.getElementById('c4');
 c4.addEventListener('click', cor4);
 
 function cor1(){
-    let color = "black";
-    return color;
+  const color = "black";
+  return color;
 }
 
 function cor2(){
-    let color = "red";
-    return color;
+  const color = "red";
+  return color;
 }
 
 function cor3(){
-    let color = "blue";
-    return color;
+  const color = "blue";
+  return color;
 }
 
 function cor4(){
-    let color = "green";
-    return color;
+  const color = "green";
+  return color;
 }
 
 function paint(){
-    for(i=1; i<=25; i++){
-        pixel = document.getElementById(`p${i}`) //id="p1" 
-        pixel.style.backgroundColor = cor1();
-    }   
+  for (let i=1; i<=25; i++){
+    let pixel = document.getElementById(`p${i}`)
+    pixel.style.backgroundColor = cor1();
+  }
 }
