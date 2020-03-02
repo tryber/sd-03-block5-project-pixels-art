6fe2f8193e219eb5283bcef3bc17386b5ff9b63e
ilan274 <ilan.herbach@gmail.com>
2020-03-02 16:29:12 -0300

    Code climate errors

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ pallete3.style.backgroundColor = 'blue';
 pallete4.style.backgroundColor = 'grey';
 
 // Color selector
-pallete1.addEventListener ('click', function () {
+pallete1.addEventListener ('click', function() {
   sessionStorage.setItem('color', 'black');
-  for (let i = 0; i < allColors.length; i+=1) {
+  for (let i = 0; i < allColors.length; i += 1) {
     if (i === 0) {
       allColors[i].className += ' selected';
     } else {
@@ -26,9 +26,9 @@ pallete1.addEventListener ('click', function () {
     }
   }
 });
-pallete2.addEventListener ('click', function () {
+pallete2.addEventListener ('click', function() {
   sessionStorage.setItem('color', 'red');
-  for (let i = 0; i < allColors.length; i+=1) {
+  for (let i = 0; i < allColors.length; i+=1 ) {
     if (i === 1) {
       allColors[i].className += ' selected';
     } else {
@@ -36,7 +36,7 @@ pallete2.addEventListener ('click', function () {
     }
   }
 });
-pallete3.addEventListener ('click', function () {
+pallete3.addEventListener ('click', function() {
   sessionStorage.setItem('color', 'blue');
   for (let i = 0; i < allColors.length; i+=1) {
     if (i === 2) {
@@ -46,7 +46,7 @@ pallete3.addEventListener ('click', function () {
     }
   }
 });
-pallete4.addEventListener ('click', function () {
+pallete4.addEventListener ('click', function() {
   sessionStorage.setItem('color', 'grey');
   for (let i = 0; i < allColors.length; i+=1) {
     if (i === 3) {
@@ -67,7 +67,7 @@ for (let i = 0; i < pixelSize; i+=1) {
 }
 
 for (let i = 0; i < pixelSize; i+=1) {
-  getPixel[i].addEventListener ('click', function () {
+  getPixel[i].addEventListener ('click', function() {
     getPixel[i].style.backgroundColor = sessionStorage.color;
   });
 }
