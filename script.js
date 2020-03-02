902d69eaa2db557362b01b7ce929a37e996298cd
Gustaff <gustavohmfigueiredo@gmail.com>
2020-03-02 15:14:55 -0300

    codecimate organizado, onload dentro do arquivo script.js, dentro da tag script.

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 function choseColor(event) {
-  let color = event.target.classList[1];
+  const color = event.target.classList[1];
   window.sessionStorage.setItem('color', color);
 }
 
 function Selected() {
-  let last = document.getElementsByClassName('selected')[0];
+  const last = document.getElementsByClassName('selected')[0];
   last.classList.remove('selected');
   event.target.classList.add('selected');
 }
 
 function insertColor(event) {
-  let classColor = sessionStorage.color;
+  const classColor = sessionStorage.color;
   event.target.style.backgroundColor = classColor;
 }
+
+function LOADED() {
+  const colorsParent = document.getElementById('color-palette');
+  colorsParent.addEventListener('click', choseColor);
+  colorsParent.addEventListener('click', Selected);
+  window.sessionStorage.setItem('color', 'black');
+
+  const square = document.getElementById('pixel-board');
+  square.addEventListener('click', insertColor);
+}
+
+window.onload = LOADED();
