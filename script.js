97c670c605c7066e0c0230fc382a79eb3da84d5d
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-02 18:27:37 -0300

    code climate_18.27

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-let c1 = document.getElementById('c1');
-c1.addEventListener('click', cor1);
-
-let c2 = document.getElementById('c2');
-c2.addEventListener('click', cor2);
-
-let c3 = document.getElementById('c3');
-c3.addEventListener('click', cor3);
-
-let c4 = document.getElementById('c4');
-c4.addEventListener('click', cor4);
-
-function cor1(){
-  const color = "black";
+function cor1() {
+  const color = 'black';
   return color;
 }
-
-function cor2(){
-  const color = "red";
+  
+function cor2() {
+  const color = 'red';
   return color;
 }
-
-function cor3(){
-  const color = "blue";
+  
+function cor3() {
+  const color = 'blue';
   return color;
 }
-
-function cor4(){
-  const color = "green";
+  
+function cor4() {
+  const color = 'green';
   return color;
 }
-
-function paint(){
+  
+function paint() {
   for (let i=1; i<=25; i++){
     let pixel = document.getElementById(`p${i}`)
     pixel.style.backgroundColor = cor1();
   }
 }
+
+const c1 = document.getElementById('c1');
+c1.addEventListener('click', cor1);
+
+const c2 = document.getElementById('c2');
+c2.addEventListener('click', cor2);
+
+const c3 = document.getElementById('c3');
+c3.addEventListener('click', cor3);
+
+const c4 = document.getElementById('c4');
+c4.addEventListener('click', cor4);
