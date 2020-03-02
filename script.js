a20658667ea0d6152c144537f985b78d7c63c914
Gabriel Lucas <dreadpool38@gmail.com>
2020-03-02 11:23:30 -0300

    algumas mudanças

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-  let selectedColor = window.getComputedStyle(document.querySelector('.selected'),null).getPropertyValue("background-color");
+
+let selectedColor = window.getComputedStyle(document.querySelector('.selected'),null).getPropertyValue("background-color");
 
 
 function getColor(e){
+
+
   selectedColor = window.getComputedStyle(e.target,null).getPropertyValue("background-color");
-  console.log(selectedColor);
+  e.target.className = "color selected"
+}
+
+function setColor(e){
+  e.target.style.backgroundColor = selectedColor;
+
 }
 
 let colorNodes = document.getElementsByClassName("color");
+let boardNodes = document.getElementsByClassName("pixel");
 
 for (node of colorNodes){
-  console.log(node)
   node.addEventListener('click', getColor)
 }
+for (pixel of boardNodes){
+    pixel.addEventListener('click', setColor)
+  }
