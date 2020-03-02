b6ced5e7ce72f91c294a3b175f03a03c50b07c40
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-03-02 19:17:15 -0300

    request seven black selected

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let selecOne = document.getElementsByClassName("black-palet");
+let selecTwo = document.getElementsByClassName("green-palet");
+let selecThree = document.getElementsByClassName("yellow-palet");
+let selecFour = document.getElementsByClassName("red-palet");
+// let corSel = " ";
+
+selecOne.addEventListener("click", change); 
+// selecTwo.addEventListener("click" changeTwo); 
+// selecThree.addEventListener("click" changeThree); 
+// selecFour.addEventListener("click" changeFour); 
+
+function change () {
+    let corSel = style.background-color
+}
+
+
+
+// for (let i = 0; i < selector.length; i++) {
+//         selector[i].addEventListener("click", function () {
+//             let corSel = selector[i].style.color;
+//     }
+//     )
+//     }
+
+
+// let botao = document.querySelectorAll("button");
+//                 for (let i = 0; i < botao.length ; i++) {
+//                    botao[i].addEventListener("click", function() {
+//                     let corT = botao[i].innerHTML;
+//                     localStorage.setItem("Cor", corT);
+//                     document.body.style.backgroundColor = localStorage.getItem("Cor")
