acf8ac2ca70d254a646c7b03963afb7eac28aa2e
tales <talestales@gmail.com>
2020-03-02 19:42:27 -0300

    wqe

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-function mudaParaVermelho(td) {
-    td.style.backgroundColor = "#FF0F0F";
+/*function mudaParaVermelho(td) {
+  td.style.backgroundColor = "#FF0F0F";
 }
 
 function mudaParaAmarelo(td) {
-    td.style.backgroundColor = "#FFFA00";
+  td.style.backgroundColor = "#FFFA00";
 }
 
 function mudaParaAzul(td) {
-    td.style.backgroundColor = "0000ff";
+  td.style.backgroundColor = "#0000ff";
 }
 
 function mudaParaPreto(td) {
-    td.style.backgroundColor = "000000";
+  td.style.backgroundColor = "#000000";
 }
-window.onload = function(){
+*/
 
-    let section =  document.querySelector("#color-palette");
-    section.addEventListener('click', function(event){
-        let selectedColor = document.querySelector(".selected1");
-        selectedColor.innerText = event.target.className;
-        classList.remove("selected");
-        selectedColor.classList.add("selected");
-    });
-        
-}
+
+  const section = document.querySelectorAll('.color');
+  for (let i = 0; i < section.length; i+=1){
+  section[i].addEventListener('click', function (event){
+    const selectedColor = document.querySelector('.selected1');
+    selectedColor.innerText = event.target.className;
+    document.querySelector('.selected').classList.remove('selected');
+    section[i].classList.add('selected');
+  }) 
+};
+const cor = document.querySelector('.selected').backgroundColor;
+const celula = document.querySelectorAll('.pixel');
+for (let i = 0; i < celula.length; i+=1){
+celula[i].addEventListener('click', function(){
+celula[i].style.backgroundColor = cor;
+});
+};
