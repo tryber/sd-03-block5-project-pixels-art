9c93bc3ee73605c9e1af0ea327464de01bab57d7
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-03-02 09:07:53 -0300

    test

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 window.onload= function(){
-    
+    let cor= document.querySelectorAll(".color")
+    sessionStorage.setItem("Cor", "black")
+    function trocaCor(){
+        let tabela= document.querySelectorAll(".pixel")
+        tabela=sessionStorage.getItem("cor").value
+        function clique(){
+        
+        cor.style.backgroundColor = sessionStorage.getItem("cor").value
+        }
+        
+        tabela.addEventListener("click", clique)
+    }
+    cor.addEventListener("click", trocaCor)
 }
