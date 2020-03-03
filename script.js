a13fba2146a08fc3bd7a77fd13e51f48971e01dc
Luma Arruda <luma.oarruda@gmail.com>
2020-03-03 19:42:47 -0300

    revertendo alteraçoes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let colorSelector = document.getElementsByClassName('color');
 let selectedColor;
-const pixelColor = document.getElementsByClassName('pixel');
-let i = 0;
+let pixelColor = document.getElementsByClassName('pixel');
 
 sessionStorage.setItem('color', 'black');
 
 // Seletor de cores - Paleta
-const selectionPalette = function (e) {
-  selectedColor = window.getComputedStyle(e.target).getPropertyValue('background-color')
+let selectionPalette = function(e) {
+    selectedColor = window.getComputedStyle( e.target ).getPropertyValue( 'background-color' )
+    console.log(`a cor selecionada é ${selectedColor}`)
 };
 
 window.onload = function () {
-  for (i < colorSelector.length; i+= 1) {
-    colorSelector[i].addEventListener('click', selectionPalette, false);
-  };
+  for (let i = 0; i < colorSelector.length; i++) {
+    colorSelector[i].addEventListener('click', selectionPalette, false)
+  }
 
-  for (i < pixelColor.length; i += 1) {
+  for (let i = 0; i < pixelColor.length; i += 1) {
     pixelColor[i].addEventListener('click', function () {
       pixelColor[i].style.backgroundColor = selectedColor;
     });
-  };
+  }
 }
