0a8a3d22be771fa2218dbc0e7f61f75fb9993494
renan.andreolle <renandreolle@hotmail.com>
2020-03-03 20:12:50 -0300

    Ajustando nomes e funcoes js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+    paletter();
     let n=5;
 for (let i=0;i<n;i++){
     procreatetr();
@@ -20,4 +21,21 @@ function procreatetd () {
         td.style.backgroundColor = getComputedStyle(select).backgroundColor;        
     })
      document.getElementById("content").lastChild.appendChild(td);
+}
+function paletter (){
+    let pal = document.getElementsByClassName("color")
+    for(let i in pal.length){
+        pal[i].addEventListener("click",function(){
+            if(pal[i].classList.length===2){
+                for(let j in pal.length){
+                    if(i===j){
+                        pal[j].classList.add("selected")
+                    }
+                    else {
+                        pal[j].classList.remove("selected")
+                    }
+                }
+            }
+        })
+    }
 }
