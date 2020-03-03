217b026782afb3caf1a9594ca18818d3cc5037f9
Hfreitas <hfreitas86@gmail.com>
2020-03-03 16:33:56 -0300

    correções de code climate

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let palette = document.querySelectorAll(".color");
-for (let i = 0; i < palette.length; i++) {
-  palette[i].addEventListener("click", function() {
-    document.querySelector(".selected").classList.remove("selected");
-    palette[i].classList.add("selected");
+const palette = document.querySelectorAll('.color');
+for (let i = 0; i < palette.length; i+=1) {
+  palette[i].addEventListener('click', function() {
+    document.querySelector('.selected').classList.remove('selected');
+    palette[i].classList.add('selected');
   });
 }
 
-let pixel = document.querySelectorAll(".pixel");
-for (let i = 0; i < pixel.length; i++) {
-  pixel[i].addEventListener("click", function() {
-    let pixelColor = document.querySelector(".selected");
+let pixel = document.querySelectorAll('.pixel');
+for (let i = 0; i < pixel.length; i+=1) {
+  pixel[i].addEventListener('click', function() {
+    let pixelColor = document.querySelector('.selected');
     pixel[i].style.backgroundColor = pixelColor.id;
   });
 }
+
