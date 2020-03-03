9c969e8a7d3e06e8e99371e0f3aafbae4c5fbc39
saturnian99 <saturnian.dsg@gmail.com>
2020-03-03 16:18:40 -0300

    Implemented 'selected' function and 'paintPixel' function

---
+++ script.js
@@ -0,0 +1,20 @@
+const allColors = document.getElementsByClassName('color');
+const pixel = document.getElementsByClassName('pixel');
+let currentColor = 'black';
+
+function getColor(color, option) {
+    // Changes current color (for painting)
+    let selectedColor = document.getElementById(option);
+    currentColor = color;
+    // Remove 'selected' class from all colors
+    for (i in allColors) {
+        allColors[i].className = 'color';
+    }
+    // Add 'selected' to current color
+    selectedColor.classList.add('selected')
+}
+
+function paintPixel(id) {
+    let clickedPixel = document.getElementById(id);
+    clickedPixel.style.backgroundColor = currentColor;
+}
