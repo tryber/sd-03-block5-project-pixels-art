e01734ddf49437aac2ad1761476964bee9e9437b
luis.campos <gucampos845@gmail.com>
2020-03-03 17:36:18 -0300

    Segunda tentativa

--- script.js
+++ script.js
@@ -1,20 +1,73 @@
-let divPale = document.getElementById('color-palette');
-let corPreta = document.getElementById('preto');
-let corVermelha = document.getElementById('vermelho');
-let corRoxa = document.getElementById('roxo');
-let corCyan = document.getElementById('cyano');
+// Cores selecionaveis
+const paleta = [
+    document.getElementById('preto').backgroundColor = "black",
+    document.getElementById('vermelho').backgroundColor = "red",
+    document.getElementById('roxo').backgroundColor = "purple",
+    document.getElementById('cyano').backgroundColor = "darkCyan",
+]
+const cor = document.querySelectorAll('.color');
 
+for (let i in paleta.length){
+    cor[i].style.backgroundColor = paleta[i];
+}
+
+cor[0].classList.add('selected');
+sessionStorage.setItem('color', cor[0].style.backgroundColor);
+
+for (let indice in paleta.length) {
+    cor[indice].addEventeListener('click', function () {
+        document.querySelector('.selected').classList.remove('.selected');
+        cor[indice].classList.add('.selected');
+        sessionStorage.setItem('color', cor[indice].style.backgroundColor);
+    })
+}
+
+const caixasBranca = document.querySelectorAll('.pixel');
+const caixa = document.querySelectorAll('.pixel');
+
+for (let i in caixasBranca.length) {
+    caixa[i].addEventeListener('click', function () {
+        caixa[i].style.backgroundColor = sessionStorage.color;
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* // Classe que seleciona a cor
 let select = document.getElementsByClassName('selected')[0];
-let caixasBranca = document.querySelectorAll('pixel');
+
+// Pixels brancos
+const caixasBranca = document.querySelectorAll('.pixel').length;
 
 corPreta.addEventListener('click', function () {
-    corPreta.value = "black";
-    alert(corPreta.value);
+    select.value = "black";
+    alert(select.innerHTML.length + 1);
 })
 
 corVermelha.addEventListener('click', function () {
-    corVermelha.value = "red";
-    alert(corVermelha.value);
+    select.value = "red";
+    alert(select.innerHTML.length + 1);
 })
 
 corRoxa.addEventListener('click', function () {
@@ -25,19 +78,4 @@ corRoxa.addEventListener('click', function () {
 corCyan.addEventListener('click', function () {
     corCyan.value = "darkCyan"
     alert(corCyan.value);
-})
-
-switch (onclick) {
-    case 'black':
-        console.log('teste');
-        break;
-
-    case 'red':
-        break;
-
-    case 'purple':
-        break;
-
-    case 'blackcyan':
-        break;
-}
+}) */
