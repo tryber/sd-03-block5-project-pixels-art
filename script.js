cd00fe391191d71883f86859e79ad921418107ef
Fernando Luckesi <fernando.luckesi94@gmail.com>
2020-03-03 16:19:05 -0300

    Projeto finalizado sem o Bonus

--- script.js
+++ script.js
@@ -7,3 +7,35 @@ function backgroundColorPaletaDeCores() {
 }
 
 backgroundColorPaletaDeCores();
+
+const corPreta = document.getElementById('black');
+const corVermelha = document.getElementById('red');
+const corAzul = document.getElementById('blue');
+const corVerde = document.getElementById('green');
+
+corPreta.classList.add('selected');
+let corSelecionada = document.querySelector('.selected');
+
+function selecionarCor() {
+  for (let i = 0; i < paletaDeCores.children.length; i += 1) {
+    paletaDeCores.children[i].addEventListener('click', () => {
+      for (let x = paletaDeCores.children.length - 1; x >= 0; x -= 1) {
+        paletaDeCores.children[x].classList.remove('selected');
+      }
+      paletaDeCores.children[i].classList.add('selected');
+      corSelecionada = document.querySelector('.selected');
+    });
+  }
+}
+
+selecionarCor();
+
+const quadroDePixels = document.getElementById('pixel-board');
+
+function backgroundColorQuadroDePixels() {
+  quadroDePixels.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = corSelecionada.id;
+  });
+}
+
+backgroundColorQuadroDePixels();
