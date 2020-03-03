57f54527a7cdaf1029ba12deebf6178190d5ac1f
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-03 19:01:46 -0300

    update req 19.01

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const paleta = document.getElementsByClassName('color');
 const blocos = document.getElementsByClassName('pixel');
 const clear = document.getElementById('clear-board');
 
-//https://www.w3schools.com/jsref/event_target.asp
-//https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
+// https://www.w3schools.com/jsref/event_target.asp
+// https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 
 function cls() {
   for (const i of blocos)
-  clear.addEventListener('click', function() {
-    i.style.background = 'white'
-    cor = '';
-  });
+    clear.addEventListener('click', function() {
+      i.style.background = 'white'
+      cor = '';
+    });
 }
 
 function select() {
@@ -19,10 +19,7 @@ function select() {
     i.addEventListener('click', function(e) {
       const pick = document.getElementById(e.target.id);
       const selecionada = window.getComputedStyle(pick).getPropertyValue('background-color');
-      cor = selecionada;
-
-      c5 = document.getElementsByClassName('.selected');
-      // c5.style.backgroundColor = selecionada;
+      cor = selecionada;      
     });
   }
 }
@@ -31,7 +28,7 @@ function paint() {
   for (const i of blocos)
       i.addEventListener('click', function () {
         i.style.background = cor;
-  });
+      });
 }
 
 select();
