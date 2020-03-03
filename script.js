591a233449b88dbc80e781dabfc98d346bd9b006
Gabryel Ryba <gabryel.ryba@gmail.com>
2020-03-03 18:38:35 -0300

    clear button feature

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let colorArr = document.getElementsByClassName('color');
 
 let pixelArr = document.getElementsByClassName('pixel');
 
+let clearBtn = document.getElementById('clear-board');
+
 for (i = 0; i<colorArr.length; i+= 1) {
     colorArr[i].addEventListener('click', select);
 }
@@ -9,6 +11,8 @@ for (i = 0; i<colorArr.length; i+= 1) {
 for (i = 0; i < pixelArr.length; i += 1) {
     pixelArr[i].addEventListener('click', paint);
 }
+
+clearBtn.addEventListener('click', clear);
 
 function select () {
     for (i = 0; i < colorArr.length; i+= 1) {
@@ -21,4 +25,10 @@ function paint() {
     let color = document.querySelector('.selected');
     
     event.target.style.backgroundColor = color.id;
+}
+
+function clear() {
+    for (i = 0; i < pixelArr.length; i+= 1) {
+        pixelArr[i].style.backgroundColor = 'white';
+    }
 }
