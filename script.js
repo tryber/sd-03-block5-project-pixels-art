bfe31d5dc187151ad8a1b60436d84067d5d2b25a
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-03 14:23:24 -0300

    making codeclimate changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const allPalettes = document.querySelectorAll('.color').length;
 const onePalette = document.querySelectorAll('.color');
 
 function defineColorPalette() {
-  for (let i = 0; i < allPalettes; i+= 1) {
+  for (let i = 0; i < allPalettes; i += 1) {
     const colors = ['black', 'orange', 'purple', 'blue'];
     onePalette[i].style.backgroundColor = colors[i];
   }
@@ -13,17 +13,17 @@ function defineColorPalette() {
 onePalette[0].classList.add('selected');
 sessionStorage.setItem('color', onePalette[0].style.backgroundColor);
 
-for (let i = 0; i < allPalettes; i+= 1) {
+for (let i = 0; i < allPalettes; i += 1) {
   defineColorPalette();
-  onePalette[i].addEventListener('click', function() {
+  onePalette[i].addEventListener('click', function () {
     document.querySelector('.selected').classList.remove('selected');
     onePalette[i].classList.add('selected');
     sessionStorage.setItem('color', onePalette[i].style.backgroundColor);
   });
 }
 
-for (let i = 0; i < allPixels; i+= 1) {
-  onePixel[i].addEventListener('click', function() {
+for (let i = 0; i < allPixels; i += 1) {
+  onePixel[i].addEventListener('click', function () {
     onePixel[i].style.backgroundColor = sessionStorage.color;
   });
 }
