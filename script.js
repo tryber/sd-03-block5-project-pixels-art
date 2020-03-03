a376bf92acb86064ab905383af1985cbaadeb7a0
Thiago Tavares <thiagooo15@gmail.com>
2020-03-03 18:55:08 -0300

    limpa tela

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const pixel = document.getElementsByClassName("pixel");
 const clear = document.getElementById("clear-board");
 
 function mudaSelected(c) {
-  for (let i = 0; i < coresArr.length; i += 1)
+  for (let i = 0; i < coresArr.length; i += 1) {
     coresArr[i].classList.remove("selected");
+  }
   const name = "selected";
   const arr = coresArr[c].className.split(" ");
   if (arr.indexOf(name) === -1) {
     coresArr[c].className += " " + name;
   }
 }
+
+function limpaTela() {
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = "white";
+  }
+}
+
+clear.addEventListener("click", limpaTela);
 
 function paint() {
   let color = document.querySelector(".selected");
