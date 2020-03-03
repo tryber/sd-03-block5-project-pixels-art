a743bfb33e2ae4581a4d0a41b8616e47eeb367c8
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-03 12:55:56 -0300

    renaming classes

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 const allPixels = document.querySelectorAll(".pixel").length;
 const onePixel = document.querySelectorAll(".pixel");
-const pallete1 = document.querySelectorAll(".color")[0];
-const pallete2 = document.querySelectorAll(".color")[1];
-const pallete3 = document.querySelectorAll(".color")[2];
-const pallete4 = document.querySelectorAll(".color")[3];
+const palette1 = document.querySelectorAll(".color")[0];
+const palette2 = document.querySelectorAll(".color")[1];
+const palette3 = document.querySelectorAll(".color")[2];
+const palette4 = document.querySelectorAll(".color")[3];
 
-pallete1.style.backgroundColor = "black";
-pallete2.style.backgroundColor = "orange";
-pallete3.style.backgroundColor = "purple";
-pallete4.style.backgroundColor = "lightblue";
+palette1.style.backgroundColor = "black";
+palette2.style.backgroundColor = "orange";
+palette3.style.backgroundColor = "purple";
+palette4.style.backgroundColor = "lightblue";
 
 sessionStorage.setItem("color", "black");
-pallete1.classList.add("selected");
+palette1.classList.add("selected");
 
-pallete1.addEventListener("click", function() {
+palette1.addEventListener("click", function() {
   sessionStorage.setItem("color", "black");
 });
 
-pallete2.addEventListener("click", function() {
+palette2.addEventListener("click", function() {
   sessionStorage.setItem("color", "orange");
 });
 
-pallete3.addEventListener("click", function() {
+palette3.addEventListener("click", function() {
   sessionStorage.setItem("color", "purple");
 });
 
-pallete4.addEventListener("click", function() {
+palette4.addEventListener("click", function() {
   sessionStorage.setItem("color", "lightblue");
 });
 
