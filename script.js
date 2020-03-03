d85366950fbc43af34acf7f8f082b7ecad188ead
natox <alvares.renato@hotmail.com>
2020-03-03 15:20:18 -0300

    Selected class fix

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const n = 7;
+
+sessionStorage.color
+const n = 5;
 function makeGrid(n) {
   const where = document.getElementById('pixel-board');
   where.appendChild(document.createElement('table'))
   for (let i = 0; i < n; i++) {
-    let col= document.createElement('td');
+    let col = document.createElement('td');
     const table = document.getElementsByTagName('table')[0]
     col.id = 'col_' + JSON.stringify(i);
 
@@ -17,21 +19,30 @@ function makeGrid(n) {
     }
   }
 }
+
 makeGrid(n)
 const buttons = document.getElementsByTagName('button')
 
-for(let i =0; i < buttons.length; i++){
-  buttons[i].addEventListener('click', function(){
-  const selectedColor = buttons[i].style.backgroundColor;
-  console.log(selectedColor);
-  sessionStorage.color = selectedColor;
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function () {
+
+    for (let b = 0; b < buttons.length; b++ ){
+      buttons[b].classList.remove('selected')
+    } buttons[i].classList.add('selected')
+    const selectedColor = buttons[i].style.backgroundColor;
+    console.log(selectedColor);
+    /*     for (let i in buttons){
+          i.classList.remove("selected")}
+          */
+    sessionStorage.color = selectedColor;
+    buttons[!i].classList.remove('selected')
   })
 }
 const pix = document.getElementsByClassName('pixel');
 
-for(let i =0; i < pix.length; i++){
-  pix[i].addEventListener('click', function(){
-  pix[i].style.backgroundColor = sessionStorage.color;
-  console.log(selectedColor)
+for (let i = 0; i < pix.length; i++) {
+  pix[i].addEventListener('click', function () {
+    pix[i].style.backgroundColor = sessionStorage.color;
+    console.log(selectedColor)
   })
 }
