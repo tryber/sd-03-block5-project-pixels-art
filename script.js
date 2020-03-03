ffa9e6c8adfcd01672b2e6a6dc641957355fdf6d
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-03-03 13:57:13 -0300

    refactoring js

--- script.js
+++ script.js
@@ -1,44 +1,29 @@
 const allPixels = document.querySelectorAll('.pixel').length;
 const onePixel = document.querySelectorAll('.pixel');
-const palette1 = document.querySelectorAll('.color')[0];
-const palette2 = document.querySelectorAll('.color')[1];
-const palette3 = document.querySelectorAll('.color')[2];
-const palette4 = document.querySelectorAll('.color')[3];
+const allPalettes = document.querySelectorAll('.color').length;
+const onePalette = document.querySelectorAll('.color');
 
-palette1.style.backgroundColor = 'black';
-palette2.style.backgroundColor = 'orange';
-palette3.style.backgroundColor = 'purple';
-palette4.style.backgroundColor = 'lightblue';
+function defineColorPalette() {
+  for (i = 0; i < allPalettes; i++) {
+    const colors = ['black', 'orange', 'purple', 'lightblue'];
+    onePalette[i].style.backgroundColor = colors[i];
+  }
+}
 
 sessionStorage.setItem('color', 'black');
-palette1.classList.add('selected');
+onePalette[0].classList.add('selected');
 
-palette1.addEventListener('click', function() {
-  sessionStorage.setItem('color', palette1.style.backgroundColor);
-});
-
-palette2.addEventListener('click', function() {
-  sessionStorage.setItem('color', palette2.style.backgroundColor);
-});
-
-palette3.addEventListener('click', function() {
-  sessionStorage.setItem('color', palette3.style.backgroundColor);
-});
-
-palette4.addEventListener('click', function() {
-  sessionStorage.setItem('color', palette4.style.backgroundColor);
-});
+for (let i = 0; i < allPalettes; i++) {
+  defineColorPalette();
+  onePalette[i].addEventListener('click', function() {
+    document.querySelector('.selected').classList.remove('selected');
+    onePalette[i].classList.add('selected');
+    sessionStorage.setItem('color', onePalette[i].style.backgroundColor);
+  });
+}
 
 for (let i = 0; i < allPixels; i++) {
   onePixel[i].addEventListener('click', function() {
     onePixel[i].style.backgroundColor = sessionStorage.color;
-  });
-}
-
-let colors = document.querySelectorAll('.color');
-for (let i = 0; i < colors.length; i++) {
-  colors[i].addEventListener('click', function() {
-    document.querySelector('.selected').classList.remove('selected');
-    colors[i].classList.add('selected');
   });
 }
