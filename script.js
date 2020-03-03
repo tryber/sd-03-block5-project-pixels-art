fe344af78e60a1640b72857b2e8c090a4ada7612
Estela Martins <estelaap_martins@hotmail.com>
2020-03-03 00:47:08 -0300

    Colorindo pixel-board

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+var penColour = 'black';
+
+function setPenColour(color)
+{
+    penColour = color;
+}
+
 function setPixelColor(pixel)
 {
-    pixel.style.backgroundColor = 'black';
+    pixel.style.backgroundColor = penColour;
 }
-// botao 
