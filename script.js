6ec8535f1316931507cb72a2c75d98f26cd69fba
davidepadua <davidepadua1914@gmail.com>
2020-03-02 22:02:26 -0300

    alguns ajustes 1.1.1

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ function setColor(color) {
 function paintPixel(id) {
     let paintedPixel = document.getElementById(id);
     paintedPixel.style.backgroundColor = actualColor;
-    paintedPixels.classList.add("selected");
-
 }
 
 function clearBoard() {
