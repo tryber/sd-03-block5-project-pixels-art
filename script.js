a165369b1b2129b1d0b90a75ca2620b55ad48fa4
Thiago Tavares <thiagooo15@gmail.com>
2020-03-03 18:41:39 -0300

    enxutando

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const pixel = document.getElementsByClassName("pixel");
 const clear = document.getElementById("clear-board");
 
 function mudaSelected(c) {
+  for (let i = 0; i < coresArr.length; i += 1)
+    coresArr[i].classList.remove("selected");
   const name = "selected";
   const arr = coresArr[c].className.split(" ");
   if (arr.indexOf(name) === -1) {
@@ -10,29 +12,14 @@ function mudaSelected(c) {
   }
 }
 
-function tiraSelected() {
-  for (let i = 0; i < coresArr.length; i += 1)
-  coresArr[i].classList.remove("selected");
-}
-
-mudaSelected(0);
-
 function paint() {
-  let color = document.querySelector('.selected');
+  let color = document.querySelector(".selected");
 
   event.target.style.backgroundColor = color.id;
 }
 
 for (i = 0; i < pixel.length; i += 1) {
-  pixel[i].addEventListener('click', paint);
+  pixel[i].addEventListener("click", paint);
 }
 
-// function adicionaClass(cn, i) {
-//   const name = cn, ;
-//   const arr = coresArr[i].className.split(" ");
-//   if (arr.indexOf(name) === -1) {
-//     coresArr[i].className += " " + name;
-//   }
-// }
-
-// adicionaClass(0)
+mudaSelected(0);
