f837f4b4a517f582b196fd0c78a7d435183241d3
Luma Arruda <luma.oarruda@gmail.com>
2020-03-03 18:47:30 -0300

    quadro abaixo da paleta

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let colorSelector = document.getElementsByClassName('color');
 let selectedColor;
+let pixelColor = document.getElementsByClassName('pixel') 
+
+sessionStorage.setItem('color', 'black');
 
 // Seletor de cores - Paleta
 let selectionPalette = function(e) {
@@ -10,5 +13,11 @@ let selectionPalette = function(e) {
 window.onload = function () {
   for (var i = 0; i < colorSelector.length; i++) {
     colorSelector[i].addEventListener('click', selectionPalette, false)
+  }
+
+  for (let i = 0; i < pixelColor.length; i += 1) {
+    pixelColor[i].addEventListener('click', function () {
+      pixelColor[i].style.backgroundColor = sessionStorage.color;
+    });
   }
 }
