1f47c3bbdf149bf5fafc1e577ef0707a2a987c91
rogerfaustino <roger_faustino@hotmail.com>
2020-03-03 20:05:01 -0300

    Codeclimate issues

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 window.onload = function () {
   function backgroundColorCell() {
-      for (let i = 0; i < 25; i+=1) {
-          const pixelSelect = document.getElementsByTagName('td')[i];
-          pixelSelect.addEventListener('click', function () {
-              pixelSelect.style.backgroundColor = selectColor();
-          });
-      };
-  };
-  backgroundColorCell();
-  function selectColor() {
-      for (let i = 1; i <= 4; i+=1) {
-          const colorcheck = document.getElementById('color' + i).checked;
-          if (colorcheck) {
-              const colorfinal = document.getElementById('color' + i).value;
-              return colorfinal;
-          };
-      };
-  };
-  const refreshbtn = document.getElementById('clear-board');
-  refreshbtn.addEventListener('click', function clickMouseLocation() {
-      window.location.reload();
+    for (let i = 0; i < 25; i +=1 ) {
+        const pixelSelect = document.getElementsByTagName('td')[i];
+        pixelSelect.addEventListener('click', function () {
+            pixelSelect.style.backgroundColor = selectColor();
+        });
+    }
+}
+backgroundColorCell();
+function selectColor() {
+    for (let i = 1; i <= 4; i += 1) {
+        const colorcheck = document.getElementById('color' + i).checked;
+        if (colorcheck) {
+            const colorfinal = document.getElementById('color' + i).value;
+            return colorfinal;
+        }
+    }
+}
+const refreshbtn = document.getElementById('clear-board');
+refreshbtn.addEventListener('click', function clickMouseLocation() {
+    window.location.reload();
   });
 };
