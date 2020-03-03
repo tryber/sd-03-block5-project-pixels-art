fd9490e2ebd6c4ce0cfd53a4ae493ead54aa7b01
Luma Arruda <luma.oarruda@gmail.com>
2020-03-03 19:30:29 -0300

    correçoes code climate

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let colorSelector = document.getElementsByClassName('color');
 let selectedColor;
-let pixelColor = document.getElementsByClassName('pixel') 
+const pixelColor = document.getElementsByClassName('pixel');
+let i = 0;
 
 sessionStorage.setItem('color', 'black');
 
 // Seletor de cores - Paleta
-let selectionPalette = function(e) {
-    selectedColor = window.getComputedStyle( e.target ).getPropertyValue( 'background-color' )
-    console.log(`a cor selecionada é ${selectedColor}`)
+const selectionPalette = function (e) {
+  selectedColor = window.getComputedStyle(e.target).getPropertyValue('background-color')
 };
 
 window.onload = function () {
-  for (var i = 0; i < colorSelector.length; i++) {
-    colorSelector[i].addEventListener('click', selectionPalette, false)
-  }
+  for (i < colorSelector.length; i+= 1) {
+    colorSelector[i].addEventListener('click', selectionPalette, false);
+  };
 
-  for (let i = 0; i < pixelColor.length; i += 1) {
+  for (i < pixelColor.length; i += 1) {
     pixelColor[i].addEventListener('click', function () {
       pixelColor[i].style.backgroundColor = selectedColor;
     });
-  }
+  };
 }
