19d89f02bac5cf17d8531112e389289f3620e587
Hfreitas <hfreitas86@gmail.com>
2020-03-03 17:00:31 -0300

    botao clear

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ for (let i = 0; i < palette.length; i+=1) {
   });
 }
 
-let pixel = document.querySelectorAll('.pixel');
+const pixel = document.querySelectorAll('.pixel');
 for (let i = 0; i < pixel.length; i+=1) {
   pixel[i].addEventListener('click', function() {
     let pixelColor = document.querySelector('.selected');
@@ -14,3 +14,12 @@ for (let i = 0; i < pixel.length; i+=1) {
   });
 }
 
+const clearButton = document.getElementById("clear-board");
+function clear () {
+for(i = 0; i< pixel.length; i+= 1){
+pixel[i].style.backgroundColor = "white";
+}
+
+}
+
+clearButton.addEventListener('click', clear)
