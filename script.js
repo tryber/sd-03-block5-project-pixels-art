64f197bfffcf7718dee9618df4b37bad849ca236
paulo-lins <paulolins100@gmail.com>
2020-03-03 01:43:40 -0300

    funcional

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
 document.body.onload = adcElementos;
-var colorPallet = document.getElementById("color-palette") 
-var pixelBoard = document.getElementById("pixel-board") 
+var colorPallet = document.getElementById("color-palette")
+var pixelBoard = document.getElementById("pixel-board")
+var colorSelect = document.getElementsByClassName("selected")
+var colorPicked = 'black'
 
-function adcElementos () {
-    for(var i = 1; i <= 25; i++){
-        // var pixelBoard = document.getElementById("pixel-board") 
-        var pixelDiv = document.createElement("div")
-        pixelDiv.className = "pixel"
-        pixelDiv.id = `pixel` + `${i}`
-        // pixelBoard.document.createElement("div")
-        pixelBoard.appendChild(pixelDiv)
-        console.log(`pixel` + `${i}`)
-    }
+function adcElementos() {
+  for (var i = 1; i <= 25; i++) {
+    var pixelDiv = document.createElement("div")
+    pixelDiv.className = "pixel"
+    pixelDiv.id = `pixel` + `${i}`
+    pixelBoard.appendChild(pixelDiv)
+  }
 }
 
-
-var selectedColor = 'black';
-
-function pickColor (e) {
-    var idSelect = (e.target.id);
-    var colorPicked = idSelect
-    
-    // idSelect.document.getElementById($"{`idSelect`}")
-    alert(colorPicked);
+function pickColor(e) {
+  var idSelect = (e.target.id);
+  colorPicked = `${idSelect}`
+  var div = document.getElementById('colorPicked')
 }
 
-function changeColor(e){
-    var idSelect = (e.target.id);
+function changeColor(e) {
+  var pixelSelected = (e.target.id)
+  var box = document.getElementById(`${pixelSelected}`)
+  box.style.backgroundColor = colorPicked;
 
-    alert(idSelect)        
 }
-
-
-// document.addEventListener ('click', pickId)
-// document.addEventListener ('click', changeColor)
-colorPallet.addEventListener('click',pickColor)
-pixelBoard.addEventListener('click',  changeColor)
-
-
-
+colorPallet.addEventListener('click', pickColor)
+pixelBoard.addEventListener('click', changeColor)
