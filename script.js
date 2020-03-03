98568843d197a5bcb91984682078de4a14e46fa5
natox <alvares.renato@hotmail.com>
2020-03-03 15:34:04 -0300

    Clear button implemented

--- script.js
+++ script.js
@@ -26,16 +26,11 @@ const buttons = document.getElementsByTagName('button')
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
 
-    for (let b = 0; b < buttons.length; b++ ){
+    for (let b = 0; b < buttons.length; b++) {
       buttons[b].classList.remove('selected')
     } buttons[i].classList.add('selected')
     const selectedColor = buttons[i].style.backgroundColor;
-    console.log(selectedColor);
-    /*     for (let i in buttons){
-          i.classList.remove("selected")}
-          */
     sessionStorage.color = selectedColor;
-    buttons[!i].classList.remove('selected')
   })
 }
 const pix = document.getElementsByClassName('pixel');
@@ -43,6 +38,10 @@ const pix = document.getElementsByClassName('pixel');
 for (let i = 0; i < pix.length; i++) {
   pix[i].addEventListener('click', function () {
     pix[i].style.backgroundColor = sessionStorage.color;
-    console.log(selectedColor)
   })
 }
+document.getElementById('clear-board').addEventListener('click', function () {
+for(let i = 0; i < pix.length; i++){
+  pix[i].style.backgroundColor = 'white'
+}
+})
