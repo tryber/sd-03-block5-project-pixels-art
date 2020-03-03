93699d707b29e729c1875b5adc543a5286a05dd0
LulDelRey <rpgstrikered@gmail.com>
2020-03-03 16:32:53 -0300

    Clear button

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let selectedColor = "black";
 function changeColor(id) {
   document.getElementById(id).style.backgroundColor = selectedColor;
 }
+
 function changeSelectedColor(newColor) {
   selectedColor = newColor;
+}
+
+function clearBoard() {
+  let listaElem = document.querySelectorAll(".pixel");
+  for (i = 0; i < listaElem.length; i++) {
+    listaElem[i].style.backgroundColor = "white";
+  }
 }
