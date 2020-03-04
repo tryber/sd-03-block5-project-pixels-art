f0f3cf2eb0d7bfa5e561652d0a133eabe5f53538
Clayton Pereira <cpereiramt@gmail.com>
2020-03-04 16:30:56 -0400

    refatorando codigo code climate

--- script.js
+++ script.js
@@ -1,56 +1,58 @@
 let corselect;
 let elementid = 'color1';
-window.onload = function() {
+window.onload = function () {
   corselect = 'black';
   this.corAleatoria('color2');
   this.corAleatoria('color3');
   this.corAleatoria('color4');
-}
-let pixels = document.getElementsByClassName('pixel');
-let color = document.getElementsByClassName('color');
-let buttonDelete =document.getElementById('clear-board');
-let tamanhoQuadro = document.getElementById('board-size');
-let botaoTamanhoQuadro = document.getElementById('generate-board');
+};
+const pixels = document.getElementsByClassName('pixel');
+const color = document.getElementsByClassName('color');
+const buttonDelete = document.getElementById('clear-board');
+const tamanhoQuadro = document.getElementById('board-size');
+const botaoTamanhoQuadro = document.getElementById('generate-board');
 
 color[0].classList.add('selected');
-buttonDelete.addEventListener('click',function (){
-    let i =0 ;
+buttonDelete.addEventListener('click', function () {
+  let i = 0;
 
-  for(;i < pixels.length; i++) {
-      pixels[i].style.backgroundColor='rgb(255,255,255)';
-    }
+  for (;i < pixels.length;) {
+    pixels[i].style.backgroundColor = 'rgb(255,255,255)';
+    i+= 1;
+  }
 });
-let x =0 ;
+let x = 0;
 for (; x < color.length; x++) {
-  color[x].addEventListener('click', function (event) {
-  document.querySelector('.selected').classList.remove('selected');
-  elementid = event.target.getAttribute('id');
-  let element = document.getElementById(elementid);
-  let style = window.getComputedStyle(element, "");
-  event.target.classList.add('selected');
-  corselect =style.getPropertyValue("background-color");
-  console.log(event.target);
-});
+    color[x].addEventListener('click', function (event) {
+    document.querySelector('.selected').classList.remove('selected');
+    elementid = event.target.getAttribute('id');
+    const element = document.getElementById(elementid);
+    const style = window.getComputedStyle(element, '');
+    event.target.classList.add('selected');
+    corselect = style.getPropertyValue("background-color");
+    x += 1;
+  });
 }
 let i = 0;
-for(; i <pixels.length; i++){
+for (; i <pixels.length;) {
   pixels[i].addEventListener('click', function (event) {
       switch (elementid) {
-          case 'color1':
-            event.target.style.backgroundColor = "rgb(0,0,0)";
-            break;
-          case 'color2':
-            event.target.style.backgroundColor = corselect;
-            break;
-          case 'color3':
-            event.target.style.backgroundColor = corselect;
-            break;
-          case 'color4':
-            event.target.style.backgroundColor = corselect;
-            break;
-          default :
-                alert('Cor não selecionada !');
-        }});
+        case 'color1':
+          event.target.style.backgroundColor = 'rgb(0,0,0)';
+          break;
+        case 'color2':
+          event.target.style.backgroundColor = corselect;
+          break;
+        case 'color3':
+          event.target.style.backgroundColor = corselect;
+          break;
+        case 'color4':
+          event.target.style.backgroundColor = corselect;
+          break;
+        default :
+          alert('Cor não selecionada !');
+      }});
+        i+=1;
 }
 botaoTamanhoQuadro.addEventListener('click', function(){
   const quadroPixels = document.getElementById('pixel-board');
@@ -59,8 +61,8 @@ botaoTamanhoQuadro.addEventListener('click', function(){
   if(vezes<5 ) {
     vezes = 5;
   } else if (vezes > 50) {
-     vezes = 50;
-   }
+      vezes = 50;
+    }
   let i = 1;
   let j=1;
   for (; i <= vezes; i++) {
