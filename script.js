66f16276c606568c9187b89d4ace8406db1a3524
Thiago Tavares <thiagooo15@gmail.com>
2020-03-04 13:55:55 -0300

    random

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ function mudaSelected(c) {
   if (arr.indexOf(name) === -1) {
     coresArr[c].className += " " + name;
   }
-
+  //pegar o valor da cor
   const color = document.querySelector(".selected");
   const style = getComputedStyle(color);
   const backgroundColor = style.backgroundColor;
   let corAtual = backgroundColor;
-  console.log(corAtual);
-
+  //talve deletar entre esses coment
 }
+
 
 
 function limpaTela() {
@@ -43,3 +43,45 @@ for (i = 0; i < pixel.length; i += 1) {
 }
 
 mudaSelected(0);
+
+function alteraCor1() {
+  let color = "";
+  let newCor = "";
+  for (let i = 0; i < 3; i++) {
+    var sub = Math.floor(Math.random() * 256).toString(16);
+    color += sub.length == 1 ? "0" + sub : sub;
+  }
+  newCor = "#" + color;
+  coresArr[1].style.backgroundColor = newCor;
+  coresArr[1].style.color = newCor;
+}
+
+alteraCor1()
+
+function alteraCor2() {
+  let color = "";
+  let newCor = "";
+  for (let i = 0; i < 3; i++) {
+    var sub = Math.floor(Math.random() * 256).toString(16);
+    color += sub.length == 1 ? "0" + sub : sub;
+  }
+  newCor = "#" + color;
+  coresArr[2].style.backgroundColor = newCor;
+  coresArr[2].style.color = newCor;
+}
+
+alteraCor2();
+
+function alteraCor3() {
+  let color = "";
+  let newCor = "";
+  for (let i = 0; i < 3; i++) {
+    var sub = Math.floor(Math.random() * 256).toString(16);
+    color += sub.length == 1 ? "0" + sub : sub;
+  }
+  newCor = "#" + color;
+  coresArr[3].style.backgroundColor = newCor;
+  coresArr[3].style.color = newCor;
+}
+
+alteraCor3();
