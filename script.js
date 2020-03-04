a6c1326ba583252a58edb2f2c0ddaf964f6b3d80
leonardodamasceno8 <leonardodamasceno8@users>
2020-03-04 16:24:45 -0300

    Paletas e seletores

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-function getColor(color) {
-    
+let classColor = document.getElementsByClassName('color');
+let classPixel = document.getElementsByClassName('pixel');
+let setColor = 'black';
 
-    
+// sessionStorage.setItem('color', 'black');
+
+// Seletor de cores - Paleta
+let selectionPalette = function(event) {
+    setColor = window.getComputedStyle( event.target ).getPropertyValue( 'background-color');
+
+    for (let i = 0; i < classColor.length; i += 1) {
+        classColor[i].classList.remove('selected');
+    };
+    console.log(setColor)
+    event.target.classList.add('selected');
+};
+
+window.onload = function () {
+    for (let i = 0; i < classColor.length; i++) {
+        classColor[i].addEventListener('click', selectionPalette, false)
+    }
+
+    for (let i = 0; i < classPixel.length; i += 1) {
+        classPixel[i].addEventListener('click', function () {
+            classPixel[i].style.backgroundColor = setColor;
+        });
+    }
 }
