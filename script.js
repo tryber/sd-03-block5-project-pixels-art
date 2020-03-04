0b3dd1711a07fb49b6f1faf0d722dc040b754694
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-03-04 16:27:56 -0300

    aumentando quadro de pixels

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const pixel_board = document.getElementById('pixel-board');
+let pixel_board = document.getElementById('pixel-board');
 let colors_pallet = document.querySelectorAll('.color');
+let pixels;
 
 for (let i = 0; i < colors_pallet.length; i++) {
 
@@ -15,26 +16,56 @@ for (let i = 0; i < colors_pallet.length; i++) {
     });
   }
   
-  //Criando pixels
-for (let i = 0; i < 25; i++) {
-  let newdiv = document.createElement('div');
-  newdiv.className = 'pixel';
-  pixel_board.append(newdiv);
-}
+  //Criando quadro de pixels
+  function board(number){
 
-const pixels = document.querySelectorAll('.pixel');
+    if(number > 50){
+      alert('Numero maior que 50 não é permitido.');
+      return false;
+    }
+
+    let qtdPixelsFuturo = number ** 2;
+    let qtdPixelsAtual = document.querySelectorAll('.pixel').length;
+    
+   pixel_board.style.width = (number*5) * 10 + "px";
+
+   for (let i = 0; i < qtdPixelsFuturo; i++) {
+     if( qtdPixelsAtual >=  number**2){
+       break;
+      }
+      let newdiv = document.createElement('div');
+      newdiv.className = 'pixel';
+      pixel_board.append(newdiv);
+    }
+
+
+    while(document.querySelectorAll('.pixel').length > number**2){
+      document.querySelector('.pixel').parentNode.removeChild(document.querySelector('.pixel'));
+    }
+
+    pixels = document.querySelectorAll('.pixel');
+  
+    [].forEach.call(pixels, pixel => {
+      pixel.addEventListener('click', function(){
+         pixel.style.backgroundColor = selected[0].getAttribute("id");
+      });
+    });
+
+  }
+  
+  board(5);
+  
+  
 let selected = document.getElementsByClassName('selected');
 
-[].forEach.call(pixels, pixel => {
-  pixel.addEventListener('click', function(){
-     pixel.style.backgroundColor = selected[0].getAttribute("id");
-  });
-});
 
-const clear = document.getElementById('clear-board');
-
-clear.addEventListener('click', function(){
+document.getElementById('clear-board').addEventListener('click', function(){
   [].forEach.call(pixels, pixel => {
       pixel.style.backgroundColor = 'white';
   });
+});
+
+document.getElementById('generate-board').addEventListener('click', function(){
+  let qtdPixels = document.getElementById('board-size').value;
+  board(qtdPixels);
 });
