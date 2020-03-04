f615d19ff4276cf9a185694d2f9c92ec3decd05d
Jafet Henrique <jafetguerra6@gmail.com>
2020-03-04 13:45:59 -0300

    Adicionando script para deixar apenas um selected

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ window.onload = function() {
   let colorBlue = document.getElementsByClassName("blue")[0];
   let colorGreen = document.getElementsByClassName("green")[0];
   let pixel = document.querySelectorAll(".pixel");
-  pixel.forEach(element => element.addEventListener("click", () => element.style.backgroundColor = mouseColor))
   let mouseColor = "black";
+
+  for (let i = 0; i < pixel.length; i++){
+    pixel[i].addEventListener("click", function() {
+      pixel[i].style.backgroundColor = mouseColor})
+  }    
+  // for (let i of pixel){
+  //  i.addEventListener("click", (event) =>{
+  //    event.target.style.backgroundColor = mouseColor})
+  //    console.log(event)
+  // }
+
+  // pixel.forEach(element => element.addEventListener("click", () => element.style.backgroundColor = mouseColor))
+  
 
   function anyColor(color) {
     mouseColor = color;
@@ -13,27 +25,36 @@ window.onload = function() {
 
   colorBlack.addEventListener("click",() => {
     anyColor("black");
+    for (let i = 0; i < document.querySelectorAll(".color").length; i++) {
+      document.querySelectorAll(".color")[i].classList.remove("selected");
+    }
     colorBlack.classList.add("selected");
-  });
+    })
 
   colorRed.addEventListener("click", () => {
     anyColor("red");
+    for (let i = 0; i < document.querySelectorAll(".color").length; i++) {
+      document.querySelectorAll(".color")[i].classList.remove("selected");
+    }
     colorRed.classList.add("selected");
   });
 
   colorBlue.addEventListener("click",() => {
     anyColor("blue")
+    for (let i = 0; i < document.querySelectorAll(".color").length; i++) {
+      document.querySelectorAll(".color")[i].classList.remove("selected");
+    }
     colorBlue.classList.add("selected");
   });
 
   colorGreen.addEventListener("click", () => {
     anyColor("green");
+    for (let i = 0; i < document.querySelectorAll(".color").length; i++) {
+      document.querySelectorAll(".color")[i].classList.remove("selected");
+    }
     colorGreen.classList.add("selected");
   });
 
-  function hehe(element) {
-    element.style.backgroundColor = mouseColor;
-  }
   
   
 }
