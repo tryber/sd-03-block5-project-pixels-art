643a203c60d87a001ce11bd8163bcb81eec6f792
Clayton Pereira <cpereiramt@gmail.com>
2020-03-03 22:23:44 -0400

    salvando alteraçoes de inclusao de cor nos pixels e de apagar os pixels com cor

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-let corBlackSelected ;
+let corselect ;
+let elementid ="black";
+
 window.onload = function(){
-corBlackSelected = true ;
+    corselect = "black" ;
 }  
 
-pixels = document.getElementsByClassName("pixel")
+let pixels = document.getElementsByClassName("pixel");
+let color = document.getElementsByClassName("color");
+let buttonDelete =document.getElementById("clear-board");
 
+buttonDelete.addEventListener("click",function(){
+    for(i= 0; i <pixels.length; i++){   
 
+     pixels[i].style.backgroundColor="rgb(255,255,255)"
 
+    }
+
+})
+
+for(x= 0; x <color.length; x++){  
+color[x].addEventListener("click",function(event){
+      elementid = event.target.getAttribute("id");
+   let element = document.getElementById(elementid);
+   var style = window.getComputedStyle(element,"");
+   corselect =style.getPropertyValue("background-color");
+   console.log(elementid);
+})
+}
 
 for(i= 0; i <pixels.length; i++){   
     
     pixels[i].addEventListener("click", function(event){
-        if(corBlackSelected){
-         event.target.style.backgroundColor = "rgb(0,0,0)"
+        switch (elementid) {
+            case "black":
+                event.target.style.backgroundColor=corselect;    
+                break;
+            case "color1":
+                event.target.style.backgroundColor= corselect;
+                break;
+            case "color2":
+                event.target.style.backgroundColor= corselect;
+                break;
+            case "color3":
+                    event.target.style.backgroundColor= corselect;
+                    break;
+            }})
 
-        }
-
-    })
-  
 }
-
-
-
-
