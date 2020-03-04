8c27fca4462c0f73776febc78811bab27d60b568
natox <alvares.renato@hotmail.com>
2020-03-04 16:07:39 -0300

    Fix grid size and add board generator

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
 sessionStorage.color = 'black'
-let n = 5 ;
-function makeGrid(n) {
+let n = 5;
+function makeGrid(tamanho) {
   const where = document.getElementById('pixel-board');
   where.appendChild(document.createElement('table'))
-  for (let i = 0; i < n; i++) {
-    let col = document.createElement('td');
+  for (let i = 0; i < tamanho; i++) {
+    let row = document.createElement('tr');
     const table = document.getElementsByTagName('table')[0]
-    col.id = 'col_' + JSON.stringify(i);
+    row.id = 'row_' + JSON.stringify(i);
 
-    table.appendChild(col)
-    for (let j = 0; j < n; j++) {
-      let row = document.createElement('div')
-      row.className = 'pixel';
-      row.id = 'pixel_' + i + '_' + j
-      let td = document.getElementsByTagName('td')[i]
-      td.appendChild(row)
+    table.appendChild(row)
+    for (let j = 0; j < tamanho; j++) {
+      let dot = document.createElement('td')
+      dot.className = 'pixel';
+      dot.id = 'pixel_' + i + '_' + j
+      let tr = document.getElementsByTagName('tr')[i]
+      tr.appendChild(dot)
     }
   }
 }
@@ -24,14 +24,13 @@ for (let j = 0; j < n; j++) {
   let createTr = document.createElement('tr')
   let td = document.getElementsByTagName('td')[i]
   td.appendChild(createTr)
-  let tr = document.getElementsByTagName('tr')[i]
+  let tr = document.getElementsByTagName('tr')[j]
   let div = document.createElement('div')
   div.className = 'pixel';
   div.id = 'pixel_' + i + '_' + j
   tr.appendChild(div)
 
 } */
-
 makeGrid(n)
 const buttons = document.getElementsByTagName('button')
 
@@ -57,8 +56,10 @@ document.getElementById('clear-board').addEventListener('click', function () {
     pix[i].style.backgroundColor = 'white'
   }
 })
-const input = document.getElementsByTagName('input');
+const size = document.getElementById('board-size');
 document.getElementById('generate-board').addEventListener('click', function(){
- n = input[0].value
+ n = Number(size.value)
+ console.log(n, size.value)
+ document.getElementsByTagName('table')[0].remove()
  makeGrid(n)
 })
