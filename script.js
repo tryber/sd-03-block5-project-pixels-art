a6843d0042c7f71431c3a46f853d2c54112e284d
Clayton Pereira <cpereiramt@gmail.com>
2020-03-04 16:12:28 -0400

    refatorando codigo code climate

--- script.js
+++ script.js
@@ -1,83 +1,78 @@
-let corselect ;
-let elementid ="color1";
-
-window.onload = function(){
-   
-    corselect = "black" ;
-    this.corAleatoria("color2");
-    this.corAleatoria("color3");
-    this.corAleatoria("color4");
-}  
-
-let pixels = document.getElementsByClassName("pixel");
-let color = document.getElementsByClassName("color");
-let buttonDelete =document.getElementById("clear-board");
-let tamanhoQuadro = document.getElementById("board-size");
-let botaoTamanhoQuadro = document.getElementById("generate-board");
+let corselect;
+let elementid = 'color1';
+window.onload = function() {
+  corselect = 'black';
+  this.corAleatoria('color2');
+  this.corAleatoria('color3');
+  this.corAleatoria('color4');
+}
+let pixels = document.getElementsByClassName('pixel');
+let color = document.getElementsByClassName('color');
+let buttonDelete =document.getElementById('clear-board');
+let tamanhoQuadro = document.getElementById('board-size');
+let botaoTamanhoQuadro = document.getElementById('generate-board');
 
 color[0].classList.add('selected');
-buttonDelete.addEventListener("click",function(){
-    for(i= 0; i <pixels.length; i++){   
-     
-     pixels[i].style.backgroundColor="rgb(255,255,255)"
-     
+buttonDelete.addEventListener('click',function (){
+    let i =0 ;
+
+  for(;i < pixels.length; i++) {
+      pixels[i].style.backgroundColor='rgb(255,255,255)';
     }
-
-})
-
-for(x= 0; x <color.length; x++){  
-color[x].addEventListener("click",function(event){
-    document.querySelector('.selected').classList.remove('selected');
-      elementid = event.target.getAttribute("id");
-   let element = document.getElementById(elementid);
-   var style = window.getComputedStyle(element,"");
-   event.target.classList.add('selected');
-   corselect =style.getPropertyValue("background-color");
-   console.log(event.target);
-})
+});
+let x =0 ;
+for (; x < color.length; x++) {
+  color[x].addEventListener('click', function (event) {
+  document.querySelector('.selected').classList.remove('selected');
+  elementid = event.target.getAttribute('id');
+  let element = document.getElementById(elementid);
+  let style = window.getComputedStyle(element, "");
+  event.target.classList.add('selected');
+  corselect =style.getPropertyValue("background-color");
+  console.log(event.target);
+});
 }
-
-for(i= 0; i <pixels.length; i++){   
-    
-    pixels[i].addEventListener("click", function(event){
-        switch (elementid) {
-            case "color1":
-                event.target.style.backgroundColor="rgb(0,0,0)";    
-                break;
-            case "color2":
-                event.target.style.backgroundColor= corselect;
-                break;
-            case "color3":
-                event.target.style.backgroundColor= corselect;
-                break;
-            case "color4":
-                event.target.style.backgroundColor= corselect;
-                break;
-            }})
-
+let i = 0;
+for(; i <pixels.length; i++){
+  pixels[i].addEventListener('click', function (event) {
+      switch (elementid) {
+          case 'color1':
+            event.target.style.backgroundColor = "rgb(0,0,0)";
+            break;
+          case 'color2':
+            event.target.style.backgroundColor = corselect;
+            break;
+          case 'color3':
+            event.target.style.backgroundColor = corselect;
+            break;
+          case 'color4':
+            event.target.style.backgroundColor = corselect;
+            break;
+          default :
+                alert('Cor não selecionada !');
+        }});
 }
-
-botaoTamanhoQuadro.addEventListener("click", function(){
-  let quadroPixels = document.getElementById("pixel-board")
+botaoTamanhoQuadro.addEventListener('click', function(){
+  const quadroPixels = document.getElementById('pixel-board');
   let vezes = tamanhoQuadro.value;
-  quadroPixels.innerHTML="";
-  if(vezes < 5 ){
-   vezes = 5 ;
-   }else if(vezes > 50){
-   vezes = 50;
-  }
-  for(i=1; i <=vezes; i++){
-  for(j=1; j<=vezes; j++){
-   quadroPixels.appendChild(document.createElement("div")).setAttribute("class","pixel");
-  }}
-})
+  quadroPixels.innerHTML = '';
+  if(vezes<5 ) {
+    vezes = 5;
+  } else if (vezes > 50) {
+     vezes = 50;
+   }
+  let i = 1;
+  let j=1;
+  for (; i <= vezes; i++) {
+  for (; j<= vezes; j++) {
+    quadroPixels.appendChild(document.createElement('div')).setAttribute('class', 'pixel');
+   }}
+});
 
-function corAleatoria(id){
-    let r = Math.floor(Math.random()*256);          
-    let g = Math.floor(Math.random()*256);          
-    let b = Math.floor(Math.random()*256);          
-    let rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
-    this.document.getElementById(id).style.background = rgb;
-    
-
+function corAleatoria(id) {
+  const r = Math.floor(Math.random()*256);
+  const g = Math.floor(Math.random()*256);
+  const b = Math.floor(Math.random()*256);
+  const rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
+  this.document.getElementById(id).style.background = rgb;
 }
