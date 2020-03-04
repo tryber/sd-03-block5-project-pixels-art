515511473a129ab9fdded2dcadc17d221641cf73
Lucas França <luc.rangel90@gmail.com>
2020-03-04 16:28:02 -0300

    with a little help from my friends

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const quadrados = document.getElementsByTagName('td');
 let corSelecionada = 'black';
+let colors = document.getElementsByClassName("color");
 const botaoLimpa = document.getElementById('clear-board');
+const corShow = document.querySelector("#mostraSelecao");
 
-function iniciar() {
-    const colors = document.getElementsByClassName("color");
-    const corShow = document.querySelector("#mostraSelecao"); 
+
+// sessionStorage.setItem('color', 'black');
+
+function start() {
     for (const tintas of colors) {
-      tintas.addEventListener("click", function(event) {
+      tintas.addEventListener("click", function(e) {
+        for (let e = 0; e < colors.length; e += 1) {
+          colors[e].classList.remove('selected')
+        };
+        e.target.classList.add('selected');
         const paletaSelecionada = document.querySelector("#" + event.target.id);
         const corDaPaletaSelecionada = window
           .getComputedStyle(paletaSelecionada)
@@ -16,18 +23,41 @@ function iniciar() {
       });
     }
   }
-function pinta() {
+
+// let selectionPalette = function(e) {
+//   corSelecionada = window
+//    .getComputedStyle( e.target )
+//    .getPropertyValue( 'background-color' );
+  // for (let i = 0; i < colors.length; i += 1) {
+  //   corSelecionada[i].classList.remove('selected');
+  // };
+  // e.target.classList.add('selected');
+// };
+
+function paint() {
     for (const pixel of quadrados)
         pixel.addEventListener('click', function () {
             pixel.style.background = corSelecionada;
         });
 }
-function apaga() {
+function clear() {
     for (const tudo of quadrados)
     botaoLimpa.addEventListener('click', function() {
         tudo.style.background = 'white'
     });
 }
-pinta();
-iniciar();
-apaga();
+start();
+paint();
+clear();
+selectionPalette();
+
+// onePalette[0].classList.add('selected');
+// sessionStorage.setItem('color', onePalette[0].style.backgroundColor);
+// for (let i = 0; i < allPalettes; i += 1) {
+//   defineColorPalette();
+//   onePalette[i].addEventListener('click', function () {
+//     document.querySelector('.selected').classList.remove('selected');
+//     onePalette[i].classList.add('selected');
+//     sessionStorage.setItem('color', onePalette[i].style.backgroundColor);
+//   });
+// }
