bcd25e228c01272ddf01e6864292bb6b1a53b5ba
rogerfaustino <roger_faustino@hotmail.com>
2020-03-05 15:35:35 -0300

    Codeclimate issues

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
+
 window.onload = function () {
+  function selectColor() {
+    for (let i = 1; i <= 4; i += 1) {
+      const colorcheck = document.getElementById('color' + i).checked;
+      if (colorcheck) {
+        const colorfinal = document.getElementById('color' + i).value;
+        return colorfinal;
+      }
+    }
+  }
   function backgroundColorCell() {
     for (let i = 0; i < 25; i += 1) {
       const pixelSelect = document.getElementsByTagName('td')[i];
       pixelSelect.addEventListener('click', function () {
-          pixelSelect.style.backgroundColor = selectColor();
+        pixelSelect.style.backgroundColor = selectColor();
       });
     }
   }
   backgroundColorCell();
-  function selectColor() {
-    for (let i = 1; i <= 4; i += 1) {
-        const colorcheck = document.getElementById('color' + i).checked;
-        if (colorcheck) {
-            const colorfinal = document.getElementById('color' + i).value;
-            return colorfinal;
-        }
-    }
-  }
   const refreshbtn = document.getElementById('clear-board');
   refreshbtn.addEventListener('click', function clickMouseLocation() {
     window.location.reload();
