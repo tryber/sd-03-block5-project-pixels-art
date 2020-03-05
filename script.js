b16f7cc813f4f481cb793fab9280036cedb3d45d
wenderson belko <contato@wendersonbelko.com>
2020-03-05 16:15:13 -0300

    80% pfvr ;-;

---
+++ script.js
@@ -0,0 +1,11 @@
+const preto = document.getElementsByClassName('tdblack');
+const amarelo = document.getElementsByClassName('tdyellow');
+const azul = document.getElementsByClassName('tdblue');
+const verde = document.getElementsByClassName('tdgreen');
+
+const cores = [
+    "black",
+    "yellow",
+    "blue",
+    "green",
+];
