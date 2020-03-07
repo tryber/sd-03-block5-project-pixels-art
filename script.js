19bf847557ae8d849d26a614f74f9287a073c927
alcantara2509 <alcantara2509@gmail.com>
2020-03-07 17:56:39 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 let cl = document.querySelectorAll('.color');
-let pixel = document.querySelector('.pixel');
+let pixel = document.querySelectorAll('.pixel');
+let blackp = document.querySelector('.black');
+let bluep = document.querySelector('.blue');
+let redp = document.querySelector('.red');
+let greenp = document.querySelector('.green');
+let pixelBoard = document.querySelector('#pixel-board');
 //selecionar a cor criando uma class
 for (let i = 0; i < cl.length; i++) {
-
-  cl[i].addEventListener('click', function(){
+  
+  cl[i].addEventListener('click', () => {
 
     for (let i = 0; i < cl.length; i++) {
-        if (cl[i].classList.contains('selected')) {
-          cl[i].className = 'color';
+        if (blackp.classList.contains('selected')) {
+          blackp.className = 'color black';
+        }if (bluep.classList.contains('selected')) {
+          bluep.className = 'color blue';
+        }if (redp.classList.contains('selected')) {
+          redp.className = 'color red';
+        }if (greenp.classList.contains('selected')) {
+          greenp.className = 'color green';
         }
       }
-      cl[i].className = 'color selected';
+      cl[i].className += ' selected';  
 
     });
   }
 
-  pixel.addEventListener("click", function() {
-    pixel.style.backgroundColor = 'rgb(000, 000, 000)'
-  })
+  for (let i = 0; i < pixel.length; i++) {
+
+    pixel[i].addEventListener('click', () => {
+      if(blackp.classList.contains('selected')) {
+        pixel[i].style.backgroundColor = 'black';
+      } if(bluep.classList.contains('selected')) {
+        pixel[i].style.backgroundColor = 'blue';
+      } if(redp.classList.contains('selected')) {
+        pixel[i].style.backgroundColor = 'red';
+      } if(greenp.classList.contains('selected')) {
+        pixel[i].style.backgroundColor = 'green';
+      }
+  
+      });
+    }
