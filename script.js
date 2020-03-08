5ae611f657033b5b798a3e027dc6ce552a11ba53
marcosjuniorclimaco <markojuniorclimako@gmail.com>
2020-03-08 16:25:07 -0300

    criando cores aleatórias e concertando o botão clear-board

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-window.onload = function(){
-let colorSelector = document.getElementsByClassName("color");
-let selectedColor = "black";
-let pixelColor = document.getElementsByClassName("pixel");
-let clear = document.getElementById("clear-board");
-
-sessionStorage.setItem("color", "black");
-
-// Seletor de cores - Paleta
-let selectionPalette = function(e) {
-  selectedColor = window
-    .getComputedStyle(e.target)
-    .getPropertyValue("background-color");
- 
-  for (let i = 0; i < colorSelector.length; i += 1) {
-    colorSelector[i].classList.remove("selected");
+const pixels = document.querySelectorAll('.pixel');
+const paleta = document.querySelectorAll('.color');
+const cores = ['black', 'paleta-2', 'paleta-3', 'paleta-4'];
+const reset = document.querySelector('#clear-board');
+const paletaAtual = ['preta', '2', '3', '4'];
+let cor = 'black';
+function geraCor() {
+  const hexadecimais = '0123456789ABCDEF';
+  let corAleatoria = '#';
+  for (let i = 0; i < 6; i += 1) {
+    corAleatoria += hexadecimais[Math.floor(Math.random() * 16)];
   }
-
-  e.target.classList.add("selected");
-};
-
-  for (let i = 0; i < colorSelector.length; i++) {
-    colorSelector[i].addEventListener("click", selectionPalette, false);
+  return corAleatoria;
+}
+for (let i = 1; i <= 3; i += 1) {
+  const corAleatoria = geraCor();
+  paleta[i].style.backgroundColor = corAleatoria;
+  cores[i] = corAleatoria;
+}
+function removerSelected() {
+  for (let i = 0; i < paleta.length; i += 1) {
+    paleta[i].className = `color paleta-${paletaAtual[i]}`;
   }
-
-  for (let i = 0; i < pixelColor.length; i += 1) {
-    pixelColor[i].addEventListener("click", function() {
-      pixelColor[i].style.backgroundColor = selectedColor;
-    });
-  }
-
-  clear.addEventListener("click", function clearBoard() {
-    for (let i = 0; i < pixelColor.length; i += 1) {
-      pixelColor[i].style.backgroundColor = "white";
+}
+function selecionaCor() {
+  for (let i = 0; i < 4; i += 1) {
+    if (this.className === `color paleta-${paletaAtual[i]}`) {
+      removerSelected();
+      this.className = `color paleta-${paletaAtual[i]} selected`;
+      cor = cores[i];
     }
-  });
-};
+  }
+}
+function atribuirCorAoPixel() {
+  this.style.backgroundColor = cor;
+}
+for (let i = 0; i < paleta.length; i += 1) {
+  paleta[i].addEventListener('click', selecionaCor);
+}
+for (let i = 0; i < pixels.length; i += 1) {
+  pixels[i].addEventListener('click', atribuirCorAoPixel);
+}
+function resetarPixelBoard() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].className = 'pixel';
+    pixels[i].style.backgroundColor = 'white';
+  }
+}
+reset.addEventListener('click', resetarPixelBoard);
