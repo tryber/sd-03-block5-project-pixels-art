c504d7d6b883cfcd615a31019724cb094b44e04f
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-08 09:56:02 -0300

    Corrigindo erro: não usar ids como seletores no css - codeclimate

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
 let pixels = document.querySelectorAll(".pixel");
 let paleta = document.querySelectorAll(".color");
-let cores = ["black", "red", "blue", "green"];
+let cores = ["paleta-preta", "paleta-2", "paleta-3", "paleta-4"];
 let reset = document.querySelector(".botao");
-cor = "black"; 
-
+let paletaAtual = ["preta", "2", "3","4"];
+cor = "paleta-preta"; 
 function removerSelected(){
     for(let i = 0; i < paleta.length; i++){
-        paleta[i].className = "color";
+        paleta[i].className = "color "+`paleta-${paletaAtual[i]}`;
     
     }
 }
-
 for(let i = 0; i < paleta.length; i++){
     paleta[i].addEventListener("click", function(){
         console.log(paleta[i]);
         removerSelected();
-        paleta[i].className = "color " + "selected";
+        paleta[i].className = "color "+`paleta-${paletaAtual[i]}`+" "+"selected";
             cor = cores[i];
     });
 }
-   
 for(let i = 0; i < pixels.length; i++){
     pixels[i].addEventListener("click", function(){
-        console.log(pixels[i]);
-        pixels[i].className = "pixel" + " " + " " + cor; 
+        pixels[i].className = "pixel "+ cor; 
     });
 }
-
 function resetarPixelBoard(){
     for(let i = 0; i < pixels.length; i++){
         pixels[i].className = "pixel";
