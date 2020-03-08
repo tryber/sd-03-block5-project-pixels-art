1dc27f2bba8e877d125c7d5a1e862eeccb3f679a
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-08 10:52:17 -0300

    havia 127 erros no codeclimate

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ const pixels = document.querySelectorAll('.pixel');
 const paleta = document.querySelectorAll('.color');
 const cores = ['paleta-preta', 'paleta-2', 'paleta-3', 'paleta-4'];
 const reset = document.querySelector('.botao');
-const paletaAtual = ['preta', '2', '3','4'];
-cor = 'paleta-preta'; 
-function removerSelected(){
-    for(let i = 0; i < paleta.length; i++){
+const paletaAtual = ['preta', '2', '3', '4'];
+let cor = 'paleta-preta';
+function removerSelected() {
+    for(let i = 0; i < paleta.length; i++) {
         paleta[i].className = 'color '+`paleta-${paletaAtual[i]}`;
     
     }
 }
-for(let i = 0; i < paleta.length; i++){
-    paleta[i].addEventListener('click', function(){
+for(let i = 0; i < paleta.length; i++) {
+    paleta[i].addEventListener('click', function() {
         console.log(paleta[i]);
         removerSelected();
         paleta[i].className = 'color '+`paleta-${paletaAtual[i]}`+' '+'selected';
             cor = cores[i];
     });
 }
-for(let i = 0; i < pixels.length; i++){
-    pixels[i].addEventListener('click', function(){
-        pixels[i].className = 'pixel '+cor; 
+for(let i = 0; i < pixels.length; i++) {
+    pixels[i].addEventListener('click', function() {
+        pixels[i].className = 'pixel '+cor;
     });
 }
-function resetarPixelBoard(){
+function resetarPixelBoard() {
     for(let i = 0; i < pixels.length; i++){
         pixels[i].className = 'pixel';
     }
