98d1133ecb8ff1173f365db8c89be61d087c959b
Lucas Cardoso <lsc.cardosoo@gmail.com>
2020-03-08 12:41:18 -0300

    menos uma função dentro de um loop

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ function removerSelected() {
     paleta[i].className = `color paleta-${paletaAtual[i]}`;
   }
 }
+function selecionaCor (event) {
+  for(let i = 0; i < 4; i += 1){
+    if(this.className == `color paleta-${paletaAtual[i]}`){
+      removerSelected();
+      this.className = `color paleta-${paletaAtual[i]} selected`;
+      cor = cores[i];
+      console.log(cor);
+    }
+  }
+}
 for (let i = 0; i < paleta.length; i += 1) {
-  paleta[i].addEventListener('click', function () {
-    removerSelected();
-    paleta[i].className = `color paleta-${paletaAtual[i]} selected`;
-    cor = cores[i];
-  });
+  paleta[i].addEventListener('click', selecionaCor);
 }
 for (let i = 0; i < pixels.length; i += 1) {
   pixels[i].addEventListener('click', function () {
@@ -22,7 +28,7 @@ for (let i = 0; i < pixels.length; i += 1) {
   });
 }
 function resetarPixelBoard() {
-  for (let i = 0; i < pixels.length; i += 1){
+  for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].className = 'pixel';
   }
 }
