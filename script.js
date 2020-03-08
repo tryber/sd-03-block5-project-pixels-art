fbc903b4628cfb41f452433a9b1f1a0ec04c1591
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-08 06:32:30 -0300

    code climate 06.32

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ const clear = document.getElementById('clear-board');
 const c1 = document.getElementById('c1');
 c1.className = 'color cor1 selected';
 
-//https://www.w3schools.com/jsref/event_target.asp
-//https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
+// https://www.w3schools.com/jsref/event_target.asp
+// https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
 
 function cls() {
-  for (const i of blocos)
-  clear.addEventListener('click', function() {
-    i.style.background = 'white'
-    cor = '';
-  });
+  for (const i of blocos) {
+    clear.addEventListener('click', function() {
+      i.style.background = 'white';
+      cor = '';
+    });      
+  }
 }
 
 function select() {
@@ -23,17 +24,17 @@ function select() {
       const pick = document.getElementById(e.target.id);
       const selecionada = window.getComputedStyle(pick).getPropertyValue('background-color');
       cor = selecionada;
-      c1.className = 'color cor1';
       pick.classList.add('selected');
     });
   }
 }
 
 function paint() {
-  for (const i of blocos)
-      i.addEventListener('click', function () {
-        i.style.background = cor;
-      });
+  for (const i of blocos) {
+    i.addEventListener('click', function () {
+      i.style.background = cor;
+    });
+  }
 }
 
 select();
