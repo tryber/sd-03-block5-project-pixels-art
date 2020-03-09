5b9b55f21477d54dac16d978a0cc0e2b3f21525b
mirellaalves <mirella.alves@gmail.com>
2020-03-09 11:46:36 -0300

    Implementando JS

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 let corSelecionada = document.getElementsByClassName("selected");
+let coresDisponiveis = document.getElementsByClassName("color");
+let aplicaCor = document.getElementsByClassName("pixel");
+let i;
+let j;
+
+for (i = 0; i < 4; i++) {
+    function selecionaNovaCor () {
+        corSelecionada.classList.remove("selected");
+        coresDisponiveis[i].classList.add("selected");
+    }
+    coresDisponiveis[i].addEventListener("click", selecionaNovaCor);
+    for (j = 0; j < 25; j++) {
+        function preencheCor () {
+            aplicaCor[j].style.backgroundColor = corSelecionada.innerHTML;
+        }
+        aplicaCor[j].addEventListener("click", preencheCor);
+    }
+}
