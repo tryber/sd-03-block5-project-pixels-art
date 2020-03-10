c960a9db650c59eff779a5c7a124a6caebb167df
mirellaalves <mirella.alves@gmail.com>
2020-03-10 12:52:07 -0300

    Ajustes codeclimate

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let coresDisponiveis = document.querySelectorAll('.color');
+const coresDisponiveis = document.querySelectorAll('.color');
 for (let i = 0; i < coresDisponiveis.length; i += 1) {
-    coresDisponiveis[i].addEventListener('click', function() {
-        document.querySelector('.selected').classList.remove('selected');
-        coresDisponiveis[i].classList.add('selected');
-    });
+  coresDisponiveis[i].addEventListener('click', function () {
+      document.querySelector('.selected').classList.remove('selected');
+      coresDisponiveis[i].classList.add('selected');
+  });
 }
 
-let aplicaCor = document.querySelectorAll('.pixel');
+const aplicaCor = document.querySelectorAll('.pixel');
 for (let i = 0; i < aplicaCor.length; i += 1) {
-    aplicaCor[i].addEventListener('click', function() {
-        aplicaCor[i].style.backgroundColor = document.querySelector('.selected').id;
-    });
+  aplicaCor[i].addEventListener('click', function () {
+      aplicaCor[i].style.backgroundColor = document.querySelector('.selected').id;
+  });
 }
