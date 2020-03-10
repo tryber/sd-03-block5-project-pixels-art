ab4759da780e6809ac367d4fda903644f38a2416
mirellaalves <mirella.alves@gmail.com>
2020-03-10 12:58:24 -0300

    Mais ajustes codeclimate

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const coresDisponiveis = document.querySelectorAll('.color');
 for (let i = 0; i < coresDisponiveis.length; i += 1) {
   coresDisponiveis[i].addEventListener('click', function () {
-      document.querySelector('.selected').classList.remove('selected');
-      coresDisponiveis[i].classList.add('selected');
+    document.querySelector('.selected').classList.remove('selected');
+    coresDisponiveis[i].classList.add('selected');
   });
 }
 
 const aplicaCor = document.querySelectorAll('.pixel');
 for (let i = 0; i < aplicaCor.length; i += 1) {
   aplicaCor[i].addEventListener('click', function () {
-      aplicaCor[i].style.backgroundColor = document.querySelector('.selected').id;
+    aplicaCor[i].style.backgroundColor = document.querySelector('.selected').id;
   });
 }
