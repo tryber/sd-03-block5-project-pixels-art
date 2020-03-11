96bb4abb02ed3d82ec2d132195641bf1a0822a71
Gustavo <ggteodor07@gmai.com>
2020-03-10 21:04:05 -0300

    Shot to 100%

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 window.onload=function(){
-    let cores = ['black','blue','red','yellow'];
+    let cores = ['black',aleatorio(),aleatorio(),aleatorio()];
     let paleta = document.getElementsByClassName('color');
     let pixelColor = document.getElementsByClassName('pixel');
-    let  clearBoard =document.getElementById('clear-board');
+    let clearBoard =document.getElementById('clear-board');
+    let generateBoard =document.getElementById(`generate-board`);
+    
     sessionStorage.setItem('Escolha','black');
+
+    function aleatorio(){
+        let simbolo = `0123456789ABCDEF`;
+        let ale = `#`;
+        for(var i=0;i<6;i++){
+            ale += simbolo[Math.floor(Math.random()*16)];
+        }
+        return ale;
+    }
 
     for (let i = 0; i < cores.length; i+=1) {
         paleta[i].style.backgroundColor = cores[i];
@@ -28,5 +39,28 @@ window.onload=function(){
         } 
     })
 
-  
+    generateBoard.addEventListener('click',function(){
+        let n = document.getElementById('board-size').value;
+        document.querySelectorAll('.linha').forEach(e => e.remove());
+        if(n<5){
+            n = 5;
+        }
+        else if(n>50){
+            n = 50;
+        }
+        for(let i=0;i<n;i++){
+            let addDiv = document.createElement('div');
+            addDiv.className = 'linha'
+            document.getElementById('pixel-board').appendChild(addDiv);
+            for(let j=0;j<n;j++){
+                let addDiv2 = document.createElement('div');
+                addDiv2.className = 'pixel';
+                addDiv2.addEventListener('click', function () {
+                    addDiv2.style.backgroundColor = sessionStorage.getItem('Escolha');
+                })
+                document.getElementsByClassName('linha')[i].appendChild(addDiv2);
+                }  
+            }
+    })
+
 }
