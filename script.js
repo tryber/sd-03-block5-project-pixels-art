d4df2ce7f776f63c4142a77233cf3e38662ca0d2
Cezene <cezenesn@hotmail.com.br>
2020-03-24 03:26:54 -0300

    generate board

--- script.js
+++ script.js
@@ -19,5 +19,29 @@ function makeColor() {
           pixelColor[i].style.backgroundColor = 'white';
         }
     })
+    let generateBoard =document.getElementById(`generate-board`);
+    generateBoard.addEventListener('click',function(){
+        let n = document.getElementById('board-size').value;
+        document.querySelectorAll('.linha').forEach(e => e.remove());
+        if(n<5){
+            n = 5;
+        }
+        else if(n>50){
+            n = 50;
+        }
+        for(let i=0;i<n;i++){
+            let addDiv = document.createElement('div');
+            addDiv.className = 'linha'
+            document.getElementById('pixel-board').appendChild(addDiv);
+            for(let j=0;j<n;j++){
+                let addDiv2 = document.createElement('div');
+                addDiv2.className = 'pixel';
+                addDiv2.addEventListener('click', function () {
+                    addDiv2.style.backgroundColor = sessionStorage.getItem('Escolha');
+                })
+                document.getElementsByClassName('linha')[i].appendChild(addDiv2);
+                }  
+            }
+    })
 }
 console.log("hey")
