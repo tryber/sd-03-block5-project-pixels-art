167e00f2b8398dd16ede82385c3af413cd8bddb7
Cezene <cezenesn@hotmail.com.br>
2020-03-24 00:16:05 -0300

    selecting black

---
+++ script.js
@@ -0,0 +1,5 @@
+let color = document.getElementsByClassName('color');
+let pixelColor = document.getElementById('texto');
+pixelColor.addEventListener('click',function(){
+    console.log("hey");
+})
